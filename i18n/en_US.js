783b108b7c73ff5eb1fb2540221fa7c3eb86ef21
wangliping <wangliping@antvision.cn>
2017-12-15 22:05:59 +0800

    客户详情中竞品、试用、信息等标签的展示

--- i18n/en_US.js
+++ i18n/en_US.js
@@ -1152,6 +1152,7 @@ var en_US = {
     "crm.contact.list.failed": "Failed to get contact list",//获取联系人列表失败
     "crm.contact.name.length": "Please enter a name with a maximum of 50 characters",//请输入最多50个字符的姓名
     "crm.last.trace.content": "Last Trace content",//最后跟进内容
+    "crm.competing.products": "Competing products",//竞品
     // 客户管理end
 
     //客户分析 start
