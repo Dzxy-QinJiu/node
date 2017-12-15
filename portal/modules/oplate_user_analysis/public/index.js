6fa5b624a79c3ab343457e5de10726f729d31f56
ning.xu <xuning02@eefung.com>
2017-12-15 13:42:23 +0800

    代码格式

--- portal/modules/oplate_user_analysis/public/index.js
+++ portal/modules/oplate_user_analysis/public/index.js
@@ -8,7 +8,7 @@ var FilterBtn = require("../../../components/filter-btn");
 var TopNav = require("../../../components/top-nav");
 var AnalysisMenu = require("../../../components/analysis_menu");
 var AnalysisAppSelector = require("../../../components/analysis_app_selector");
-import { AntcDatePicker } from "antc";
+import { AntcDatePicker, AntcCardContainer } from "antc";
 var GeminiScrollbar = require("../../../components/react-gemini-scrollbar");
 var AnalysisLayout = require("./utils/analysis-layout");
 var OplateUserAnalysisAction = require("./action/oplate-user-analysis.action");
@@ -37,7 +37,7 @@ import {handleUserStatis, handleExportData, handlePieChartData, handleActivelyDa
      handleZoneExportData, handleDeviceExport, handleBrowserExport, handleAveTimesExport,
      handleLoginCountsExport, handleLoginDaysExport, handleLoginTimesExport} from './utils/export-data-util'
 const ChinaMap = require('CMP_DIR/china-map'); // 中国地图
-import { AntcTable, AntcCardContainer } from "antc";
+import { AntcTable } from "antc";
 var SelectFullWidth = require("../../../components/select-fullwidth");
 import ajax from "../../common/ajax";
 import routeList from "../../common/route";
@@ -1800,16 +1800,16 @@ var OPLATE_USER_ANALYSIS = React.createClass({
                     </div>
                 ), (
                     <div className="analysis_chart col-md-6 col-sm-12 charts-select-fix"
-                        data-title={Intl.get("oplate.user.analysis.averageLoginTimes", "平均在线时长")}>
+                        data-title={Intl.get("oplate.user.analysis.averageLoginTimes", "平均在线时长")}>                        
                         <div className="chart-holder">
-                        <AntcCardContainer 
-                            title={Intl.get("oplate.user.analysis.averageLoginTimes", "平均在线时长")}
-                            exportData={handleAveTimesExport(this.state.onlineTime.data)}
-                            csvFileName="average_times_statis.csv"
-                            subTitle={activeConditionSelector}
-                        >
-                            {this.getOnlineTimeChart()}
-                        </AntcCardContainer>
+                            <AntcCardContainer 
+                                title={Intl.get("oplate.user.analysis.averageLoginTimes", "平均在线时长")}
+                                exportData={handleAveTimesExport(this.state.onlineTime.data)}
+                                csvFileName="average_times_statis.csv"
+                                subTitle={activeConditionSelector}
+                            >
+                                {this.getOnlineTimeChart()}
+                            </AntcCardContainer>
                         </div>
                     </div>
                 ))
