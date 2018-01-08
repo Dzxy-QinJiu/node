afd63453ba84ebe35ad77dcd616c0d4ceadac4bd
zhshj <1062735818@qq.com>
2018-01-08 10:27:02 +0800

    日程管理遗漏对Icon的引用

--- portal/modules/schedule_management/public/views/day-agenda-schedule-lists.js
+++ portal/modules/schedule_management/public/views/day-agenda-schedule-lists.js
@@ -3,7 +3,7 @@
  * 版权所有 (c) 2016-2017 湖南蚁坊软件股份有限公司。保留所有权利。
  * Created by zhangshujuan on 2017/12/14.
  */
-import {Row, Col, Button} from "antd";
+import {Row, Col, Button, Icon} from "antd";
 import GeminiScrollbar from "CMP_DIR/react-gemini-scrollbar";
 import dates from 'date-arithmetic';
 import BigCalendar from 'react-big-calendar';
