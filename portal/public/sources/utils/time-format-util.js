1bc93462adbeaa426e48454c493e693b407cc1c2
wangliping <wangliping@antvision.cn>
2018-01-15 14:46:58 +0800

    通话统计列表中，时间计算三位数以上的小时数只展示了两位的问题修改

--- portal/public/sources/utils/time-format-util.js
+++ portal/public/sources/utils/time-format-util.js
@@ -26,12 +26,18 @@ exports.secondsToHourMinuteSecond = function (timeSeconds) {
 };
 //将数字时间改为字符串
 function getStringTime(time) {
-    return ("0" + time).slice(-2);
+    let timeStr = "" + time;
+    if (timeStr.length >= 2) {
+        return timeStr;
+    } else {//不到两位数时，前面补0
+        return ("0" + timeStr).slice(-2);
+    }
 }
 //获取01:02:00（1小时2分钟）格式的时间
 exports.getFormatTime = function (time = 0) {
     let timeObj = this.secondsToHourMinuteSecond(time);
-    let hour = getStringTime(timeObj.hours), minute = getStringTime(timeObj.minutes), second = getStringTime(timeObj.second);
+    let hour = getStringTime(timeObj.hours), minute = getStringTime(timeObj.minutes),
+        second = getStringTime(timeObj.second);
     return `${hour}:${minute}:${second}`;
 };
 //计算出不同过期时间段对应的开始时间 本天 本周 本月 半年
@@ -62,22 +68,29 @@ exports.getEndTime = function (time) {
     }
 };
 //某个日期时星期几
-exports.getCurrentWeek= function(time){
+exports.getCurrentWeek = function (time) {
     var Week = moment(time).format('dddd');
-    switch (Week){
-        case 'Monday': Week= Intl.get("schedule.user.time.monday","星期一");
+    switch (Week) {
+        case 'Monday':
+            Week = Intl.get("schedule.user.time.monday", "星期一");
             break;
-        case 'Tuesday': Week= Intl.get("schedule.user.time.tuesday","星期二");
+        case 'Tuesday':
+            Week = Intl.get("schedule.user.time.tuesday", "星期二");
             break;
-        case 'Wednesday': Week= Intl.get("schedule.user.time.wednesday","星期三");
+        case 'Wednesday':
+            Week = Intl.get("schedule.user.time.wednesday", "星期三");
             break;
-        case 'Thursday': Week= Intl.get("schedule.user.time.thursday","星期四");
+        case 'Thursday':
+            Week = Intl.get("schedule.user.time.thursday", "星期四");
             break;
-        case 'Friday': Week= Intl.get("schedule.user.time.friday","星期五");
+        case 'Friday':
+            Week = Intl.get("schedule.user.time.friday", "星期五");
             break;
-        case 'Saturday': Week= Intl.get("schedule.user.time.saturday","星期六");
+        case 'Saturday':
+            Week = Intl.get("schedule.user.time.saturday", "星期六");
             break;
-        case 'Sunday': Week= Intl.get("schedule.user.time.sunday","星期日");
+        case 'Sunday':
+            Week = Intl.get("schedule.user.time.sunday", "星期日");
             break;
         default:
             break;
