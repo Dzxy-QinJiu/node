346e3fbe510ceb79a77f310f4fec03b42d5944fe
liwenjun <liwenjun@antvision.cn>
2018-03-14 13:44:12 +0800

    提取公共常量

--- portal/modules/logout/action/desktop-logout-controller.js
+++ portal/modules/logout/action/desktop-logout-controller.js
@@ -12,7 +12,8 @@ var config = require("../../../../conf/config");
 exports.logout = function (req, res) {
     //语言环境
     var lang = req.session.lang;
-    var langParam = lang ? ("lang=" + lang) : "";
+    var langParam = lang ? ("lang=" + lang) : "";//语言参数
+    var loginUrl = "/login" + (langParam ? ("?" + langParam) : "");//登录界面路径
     if (config.useSso) {
         //如果使用sso登录的，调用ssoLogout
         DesktopLogoutService.ssoLogout(req, res).on("success", function () {
@@ -26,14 +27,14 @@ exports.logout = function (req, res) {
             //删除session的user，但不清除session,为了传递stopcheck，并需要重新登录
             req.session.user = "";
             req.session.save(function () {
-                res.redirect("/login" + (langParam ? ("?" + langParam) : ""));
+                res.redirect(loginUrl);
             });
         });
     } else {
         //普通的登录,调用logout
         DesktopLogoutService.logout(req, res);
         req.session.destroy(function () {
-            res.redirect("/login" + (langParam ? ("?" + langParam) : ""));
+            res.redirect(loginUrl);
         });
     }
 };
