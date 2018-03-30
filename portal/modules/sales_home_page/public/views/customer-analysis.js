a20d96e09a9759a79bea2ab147b4197da7429dff
xuning <xuning02@eefung.com>
2018-03-30 11:01:04 +0800

    去除无用变量

--- portal/modules/sales_home_page/public/views/customer-analysis.js
+++ portal/modules/sales_home_page/public/views/customer-analysis.js
@@ -497,7 +497,7 @@ var CustomerAnalysis = React.createClass({
                 data-tracename="转出客户统计"
                 style={{ height: (loadingFirst || this.state.transferCustomers.data.length == 0) ? "246px" : "540px" }}
             >
-                <GeminiScrollbar enabled={true} >
+                <GeminiScrollbar>
                     <div className="title"><ReactIntl.FormattedMessage id="user.analysis.moveoutCustomer"
                         defaultMessage={Intl.get("user.analysis.moveoutCustomer", "转出客户统计")} />
                     </div>
