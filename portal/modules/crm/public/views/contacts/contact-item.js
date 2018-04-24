f5839b724ea2a06623c1b712177ed02fccbcc1d9
wangliping <wangliping@eefung.com>
2018-04-24 19:36:20 +0800

    更新默认联系人方法的判断

--- portal/modules/crm/public/views/contacts/contact-item.js
+++ portal/modules/crm/public/views/contacts/contact-item.js
@@ -1,10 +1,10 @@
-import { Button, message } from "antd";
+import {Button, message} from "antd";
 var BootstrapButton = require("react-bootstrap").Button;
 var ContactUtil = require("../../utils/contact-util");
 var ContactAction = require("../../action/contact-action");
 var Modal = require("react-bootstrap").Modal;
 var Spinner = require('../../../../../components/spinner');
-import { addHyphenToPhoneNumber } from "LIB_DIR/func";
+import {addHyphenToPhoneNumber} from "LIB_DIR/func";
 import Trace from "LIB_DIR/trace";
 import crmAjax from '../../ajax/index';
 import CrmAction from "../../action/crm-actions";
@@ -21,11 +21,11 @@ var ContactItem = React.createClass({
         };
     },
     showEditContactForm: function () {
-        Trace.traceEvent(this.getDOMNode(),"编辑联系人");
+        Trace.traceEvent(this.getDOMNode(), "编辑联系人");
         ContactAction.showEditContactForm(this.props.contact);
     },
     showDeleteContactConfirm: function () {
-        Trace.traceEvent(this.getDOMNode(),"删除联系人");
+        Trace.traceEvent(this.getDOMNode(), "删除联系人");
         ContactAction.showDeleteContactConfirm(this.props.contact);
     },
     toggleDefaultContact: function () {
@@ -36,18 +36,20 @@ var ContactItem = React.createClass({
             ContactAction.toggleDefaultContact(this.props.contact.contact, (result) => {
                 this.setState({isLoading: false});
                 if (_.isObject(result) && result.code === 0) {
-                    message.success( Intl.get("crm.117", "修改默认联系人成功"));
+                    message.success(Intl.get("crm.117", "修改默认联系人成功"));
                     //修改默认联系人的信息时，更新列表中的联系人数据
                     this.props.contact.contact.def_contancts = "true";
-                    this.props.updateCustomerDefContact(this.props.contact.contact);
+                    if (_.isFunction(this.props.updateCustomerDefContact)) {
+                        this.props.updateCustomerDefContact(this.props.contact.contact);
+                    }
                 } else {
-                    message.error( Intl.get("crm.118", "修改默认联系人失败"));
+                    message.error(Intl.get("crm.118", "修改默认联系人失败"));
                 }
             });
         }
     },
     hideDeleteContactModal: function () {
-        Trace.traceEvent(this.getDOMNode(),"取消删除联系人");
+        Trace.traceEvent(this.getDOMNode(), "取消删除联系人");
         ContactAction.hideDeleteContactConfirm(this.props.contact);
     },
     deleteContact: function () {
@@ -55,7 +57,7 @@ var ContactItem = React.createClass({
             this.props.delMergeCustomerContact(this.props.contact.contact.id);
             ContactAction.hideDeleteContactConfirm(this.props.contact.contact);
         } else {
-            Trace.traceEvent(this.getDOMNode(),"确认删除联系人");
+            Trace.traceEvent(this.getDOMNode(), "确认删除联系人");
             let customerId = this.props.contact.contact.customer_id;
             ContactAction.deleteContact(this.props.contact, () => {
                 this.props.refreshCustomerList(customerId);
@@ -65,20 +67,20 @@ var ContactItem = React.createClass({
 
     // 自动拨号
     handleClickCallOut(phone) {
-        Trace.traceEvent(this.getDOMNode(),"拨打电话");
+        Trace.traceEvent(this.getDOMNode(), "拨打电话");
         if (this.props.getCallNumberError) {
-            message.error( this.props.getCallNumberError ||  Intl.get("crm.get.phone.failed", "获取座机号失败!") );
+            message.error(this.props.getCallNumberError || Intl.get("crm.get.phone.failed", "获取座机号失败!"));
         } else {
             if (this.props.callNumber) {
                 var contact = "";
-                if (this.props.contact && this.props.contact.contact){
+                if (this.props.contact && this.props.contact.contact) {
                     contact = this.props.contact.contact.name;
                 }
                 var curCustomer = this.props.curCustomer;
                 phoneMsgEmitter.emit(phoneMsgEmitter.SEND_PHONE_NUMBER,
                     {
-                        phoneNum : phone.replace('-',''),
-                        contact : contact,
+                        phoneNum: phone.replace('-', ''),
+                        contact: contact,
                         customerDetail: curCustomer,//客户基本信息
                     }
                 );
@@ -86,13 +88,13 @@ var ContactItem = React.createClass({
                     from: this.props.callNumber,
                     to: phone
                 };
-                crmAjax.callOut(reqData).then( (result) => {
+                crmAjax.callOut(reqData).then((result) => {
                     if (result.code == 0) {
                         message.success('拨打成功！');
                     }
                 }, (errMsg) => {
                     message.error(errMsg || '拨打失败！');
-                } );
+                });
             } else {
                 message.error(Intl.get("crm.bind.phone", "请先绑定分机号！"));
             }
@@ -104,17 +106,19 @@ var ContactItem = React.createClass({
         //默认联系人区域
         var defaultContactSection = contact.contact.def_contancts == "true" ? (
             <div className="crm-contact-default">
-                <span><ReactIntl.FormattedMessage id="crm.119" defaultMessage="默认" /></span>
+                <span><ReactIntl.FormattedMessage id="crm.119" defaultMessage="默认"/></span>
             </div>
         ) : null;
         //联系方式-电话
-        var contactWayPhone = contact.contact.phone && contact.contact.phone[0] ? contact.contact.phone.map( (phone) => {
+        var contactWayPhone = contact.contact.phone && contact.contact.phone[0] ? contact.contact.phone.map((phone) => {
             return (
                 <dl>
-                    <dt><ReactIntl.FormattedMessage id="common.phone" defaultMessage="电话" />：</dt>
+                    <dt><ReactIntl.FormattedMessage id="common.phone" defaultMessage="电话"/>：</dt>
                     <dd>
                         {addHyphenToPhoneNumber(phone)}
-                        {this.props.callNumber? <i className="iconfont icon-call-out call-out" title={Intl.get("crm.click.call.phone", "点击拨打电话")} onClick={this.handleClickCallOut.bind(this,phone)}></i> : null}
+                        {this.props.callNumber ? <i className="iconfont icon-call-out call-out"
+                                                    title={Intl.get("crm.click.call.phone", "点击拨打电话")}
+                                                    onClick={this.handleClickCallOut.bind(this, phone)}></i> : null}
                     </dd>
                 </dl>
             );
@@ -132,7 +136,7 @@ var ContactItem = React.createClass({
         var contactWayWeChat = contact.contact.weChat && contact.contact.weChat[0] ? contact.contact.weChat.map(function (weChat) {
             return (
                 <dl>
-                    <dt><ReactIntl.FormattedMessage id="crm.58" defaultMessage="微信" />：</dt>
+                    <dt><ReactIntl.FormattedMessage id="crm.58" defaultMessage="微信"/>：</dt>
                     <dd>{weChat}</dd>
                 </dl>
             );
@@ -141,7 +145,7 @@ var ContactItem = React.createClass({
         var contactWayEmail = contact.contact.email && contact.contact.email[0] ? contact.contact.email.map(function (email) {
             return (
                 <dl>
-                    <dt><ReactIntl.FormattedMessage id="common.email" defaultMessage="邮箱" />：</dt>
+                    <dt><ReactIntl.FormattedMessage id="common.email" defaultMessage="邮箱"/>：</dt>
                     <dd>{email}</dd>
                 </dl>
             );
@@ -166,19 +170,19 @@ var ContactItem = React.createClass({
                     <div className="crm-contact-line"></div>
                     <div className="pull-left contact-left col-md-5">
                         <dl>
-                            <dt><ReactIntl.FormattedMessage id="call.record.contacts" defaultMessage="联系人" /> : </dt>
+                            <dt><ReactIntl.FormattedMessage id="call.record.contacts" defaultMessage="联系人"/> :</dt>
                             <dd>{contact.contact.name}</dd>
                         </dl>
                         <dl>
-                            <dt>{Intl.get("crm.113", "部门")} : </dt>
+                            <dt>{Intl.get("crm.113", "部门")} :</dt>
                             <dd>{contact.contact.department}</dd>
                         </dl>
                         <dl>
-                            <dt>{Intl.get("crm.91", "职位")} : </dt>
+                            <dt>{Intl.get("crm.91", "职位")} :</dt>
                             <dd>{contact.contact.position}</dd>
                         </dl>
                         <dl>
-                            <dt>{Intl.get("user.apply.detail.table.role", "角色")} : </dt>
+                            <dt>{Intl.get("user.apply.detail.table.role", "角色")} :</dt>
                             <dd>{contact.contact.role}</dd>
                         </dl>
                     </div>
@@ -198,20 +202,22 @@ var ContactItem = React.createClass({
                             <Modal.Title />
                         </Modal.Header>
                         <Modal.Body>
-                            <p><ReactIntl.FormattedMessage id="crm.120" defaultMessage="是否删除此联系人" />？</p>
+                            <p><ReactIntl.FormattedMessage id="crm.120" defaultMessage="是否删除此联系人"/>？</p>
                         </Modal.Body>
                         <Modal.Footer>
-                            <BootstrapButton className="btn-ok" onClick={this.deleteContact}><ReactIntl.FormattedMessage id="common.sure" defaultMessage="确定" /></BootstrapButton>
+                            <BootstrapButton className="btn-ok" onClick={this.deleteContact}><ReactIntl.FormattedMessage
+                                id="common.sure" defaultMessage="确定"/></BootstrapButton>
                             <BootstrapButton className="btn-cancel"
-                                             onClick={this.hideDeleteContactModal}><ReactIntl.FormattedMessage id="common.cancel" defaultMessage="取消" /></BootstrapButton>
+                                             onClick={this.hideDeleteContactModal}><ReactIntl.FormattedMessage
+                                id="common.cancel" defaultMessage="取消"/></BootstrapButton>
                         </Modal.Footer>
                     </Modal>
 
-                {
-                    this.state.isLoading ?
-                        (<Spinner className="isloading"/>) :
-                        (null)
-                }
+                    {
+                        this.state.isLoading ?
+                            (<Spinner className="isloading"/>) :
+                            (null)
+                    }
 
                 </div>
             </div>
