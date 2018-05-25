fa47a9d94c621cf28f48064fdf4b991c4892983b
huzhenli <huzhenli@eefung.com>
2018-05-25 19:40:51 +0800

    审计日志，选择应用id的处理

--- portal/modules/app_user_manage/public/action/log-analysis-util.js
+++ portal/modules/app_user_manage/public/action/log-analysis-util.js
@@ -3,12 +3,18 @@ var ShareObj = require("../util/app-id-share-util");
 exports.handleSelectAppId = (userOwnAppList) => {
     // 上一个用户选择应用id
     let lastSelectAppId = ShareObj.share_differ_user_keep_app_id;
-    let index = _.indexOf(_.pluck(userOwnAppList, 'app_id'),lastSelectAppId);
     let selectAppId = "";
-    if (index > -1) {
-        selectAppId = lastSelectAppId;
-    } else {
-        selectAppId = userOwnAppList.length && userOwnAppList[0].app_id || '';
+    if (_.isArray(userOwnAppList) && userOwnAppList.length) {
+        if (lastSelectAppId) {
+            let index = _.indexOf(_.pluck(userOwnAppList, 'app_id'),lastSelectAppId);
+            if (index > -1) {
+                selectAppId = lastSelectAppId;
+            } else {
+                selectAppId = userOwnAppList[0].app_id;
+            }
+        } else {
+            selectAppId = userOwnAppList[0].app_id ;
+        }
     }
     return selectAppId;
 };
