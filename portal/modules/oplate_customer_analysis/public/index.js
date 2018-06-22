1ea013d9c58db8439996c165633e4ffde7c482fc
yubin <yubin@eefung.com>
2018-06-22 10:30:08 +0800

    客户分析客户及订单阶段统计处理函数改为调用分析组件里的

--- portal/modules/oplate_customer_analysis/public/index.js
+++ portal/modules/oplate_customer_analysis/public/index.js
@@ -56,89 +56,6 @@ var OPLATE_CUSTOMER_ANALYSIS = React.createClass({
             this.setState({stageList: result.result});
         });
     },
-    
-    //处理客户阶段统计数据
-    processCustomerStageData(data) {
-        const customerStages = [
-            {
-                tagName: Intl.get('sales.stage.message', '信息'),
-                tagValue: 'message',
-            },
-            {
-                tagName: Intl.get('sales.stage.intention', '意向'),
-                tagValue: 'intention',
-            },
-            {
-                tagName: Intl.get('common.trial', '试用'),
-                tagValue: 'trial',
-            },
-            {
-                tagName: Intl.get('common.qualified', '合格'),
-                tagValue: 'qualified',
-            },
-            {
-                tagName: Intl.get('sales.stage.signed', '签约'),
-                tagValue: 'signed',
-            },
-        ];
-    
-        let processedData = [];
-        let prevStageValue;
-    
-        customerStages.forEach(stage => {
-            let stageValue = data[stage.tagValue];
-    
-            if (stageValue) {
-                //保留原始值，用于在图表上显示
-                const showValue = stageValue;
-    
-                //如果下一阶段的值比上一阶段的值大，则将下一阶段的值变得比上一阶段的值小，以便能正确排序
-                if (prevStageValue && stageValue > prevStageValue) {
-                    stageValue = prevStageValue * 0.8;
-                }
-    
-                //将暂存的上一阶段的值更新为当前阶段的值，以供下一循环中使用
-                prevStageValue = stageValue;
-    
-                processedData.push({
-                    name: stage.tagName,
-                    value: stageValue,
-                    showValue,
-                });
-            }
-        });
-    
-        return processedData;
-    },
-    
-    //处理订单阶段统计数据
-    processOrderStageData(data) {
-        //接口返回数据里没有value字段，但是图表渲染需要该字段，所以需要补上该字段
-        _.map(data, stage => {
-            stage.value = stage.total;
-            if (_.isNumber(stage.budget)) {
-                //对预算额做千分位分隔及加单位处理
-                stage.budget = parseAmount(stage.budget) + Intl.get('contract.139', '万');
-            }
-        });
-    
-        const stageList = this.state.stageList || [];
-        let processedData = [];
-    
-        //将统计数据按销售阶段列表顺序排序
-        _.each(stageList, stage => {
-            const dataItem = _.find(data, item => item.name === stage.name);
-            if (dataItem) {
-                processedData.push(dataItem);
-            }
-        });
-    
-        //将维护阶段的统计数据加到处理后的数据的最后
-        let maintainStage = _.find(data, stage => stage.name === Intl.get('oplate_customer_analysis.6', '维护阶段'));
-        if (maintainStage) processedData.push(maintainStage);
-    
-        return processedData;
-    },
 
     //处理图表点击事件
     handleChartClick(name, value, conditions) {
@@ -258,7 +175,7 @@ var OPLATE_CUSTOMER_ANALYSIS = React.createClass({
             title: Intl.get('oplate_customer_analysis.customer_stage', '客户阶段统计'),
             url: '/rest/analysis/customer/stage/label/:auth_type/summary',
             chartType: 'funnel',
-            processData: this.processCustomerStageData,
+            processData: AntcAnalysis.utils.processCustomerStageData,
             customOption: {
                 valueField: 'showValue',
                 minSize: '5%',
@@ -270,7 +187,7 @@ var OPLATE_CUSTOMER_ANALYSIS = React.createClass({
             title: Intl.get('oplate_customer_analysis.11', '订单阶段统计'),
             url: '/rest/analysis/customer/v1/:auth_type/:tab/stage',
             chartType: 'horizontalStage',
-            processData: this.processOrderStageData,
+            processData: AntcAnalysis.utils.processOrderStageData,
             noShowCondition: {
                 tab: ['!', 'total'],
             },
