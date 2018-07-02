576f3aa4bcbb868d545d133f8b6ed7669ad2a817
wangliping <wangliping@eefung.com>
2018-07-02 15:42:12 +0800

    没有跟进记录和日程时提示位置的修改

--- portal/components/detail-card/index.js
+++ portal/components/detail-card/index.js
@@ -22,12 +22,18 @@ class DetailCard extends React.Component {
     }
 
     render() {
-        let className = classNames(this.props.className, 'detail-card-container', {'detail-card-edit-status': this.props.isEdit});
+        let className = classNames(this.props.className, 'detail-card-container', {
+            'detail-card-edit-status': this.props.isEdit,
+            'no-data-card': this.props.noDataTip
+        });
         //若果没有标题时，编辑状态的内容不显示border
         let contentCl = classNames('detail-card-content', {'card-content-edit-status': this.props.isEdit && this.props.title});
         return (
             <div className={className}>
-                {this.props.title ? (<div className="detail-card-title">{this.props.title}</div>) : null}
+                {this.props.title ? (<div className="detail-card-title">
+                    {this.props.title}
+                    {this.props.noDataTip ? <span className="no-data-text">{this.props.noDataTip}</span> : null}
+                </div>) : null}
                 {this.props.content ? (
                     <div className={contentCl}>
                         {this.props.content}
@@ -47,6 +53,7 @@ DetailCard.defaultProps = {
     loading: false,//是否正在保存
     okBtnText: '',//保存按钮上的描述
     cancelBtnText: '',//取消按钮上的描述
+    noDataTip: null,//没有数据时，标题后面的暂无xxx提示
     handleSubmit: function() {
     },//保存的处理
     handleCancel: function() {
@@ -54,5 +61,20 @@ DetailCard.defaultProps = {
     renderHandleSaveBtns: function() {
     },//渲染自定义的处理保存的按钮
     saveErrorMsg: ''//保存的错误提示
+};
+DetailCard.propTypes = {
+    title: React.PropTypes.string || React.PropTypes.element,
+    content: React.PropTypes.string || React.PropTypes.element,
+    bottom: React.PropTypes.string || React.PropTypes.element,
+    className: React.PropTypes.string,
+    isEdit: React.PropTypes.bool,
+    loading: React.PropTypes.bool,
+    okBtnText: React.PropTypes.string,
+    cancelBtnText: React.PropTypes.string,
+    noDataTip: React.PropTypes.string,
+    handleSubmit: React.PropTypes.func,
+    handleCancel: React.PropTypes.func,
+    renderHandleSaveBtns: React.PropTypes.func,
+    saveErrorMsg: React.PropTypes.string
 };
 export default DetailCard;
