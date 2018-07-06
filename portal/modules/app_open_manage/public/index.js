fde408bd38c168d429122abe2468084e80c2d42c
xuning <xuning02@eefung.com>
2018-07-06 10:25:02 +0800

    应用界面、样式调整

--- portal/modules/app_open_manage/public/index.js
+++ portal/modules/app_open_manage/public/index.js
@@ -1,4 +1,15 @@
+import { Button, Modal } from 'antd';
+require('./style/index.less');
 const OpenAppAction = require('./action');
+
+
+const appInfo = [
+    {
+        title: '合同管理',
+        desc: '合同管理可以帮您管理合同,统计和分析合同数据',
+        id: 'contract'
+    }
+];
 
 class OpenApp extends React.Component {
     constructor(props) {
@@ -10,10 +21,25 @@ class OpenApp extends React.Component {
     componentDidMount() {
 
     }
+    handleCheckDetail(app) {
+
+    }
     render() {
         return (
-            <div>
-                123123
+            <div className="open-app-wrapper">
+                <div className="">
+                    {
+                        appInfo.map((app, index) => (
+                            <fieldset key={index} className='app-container'>
+                                <legend>{app.title}</legend>
+                                <p>{app.desc}</p>
+                                <div className="btn-bar">
+                                    <Button onClick={this.handleCheckDetail.bind(this, app)}>查看详情</Button>
+                                </div>
+                            </fieldset>
+                        ))
+                    }
+                </div>
 
             </div>
         );
