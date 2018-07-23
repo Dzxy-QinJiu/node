5ce0cc7ed21c3617ff64e6bff3c48b4fc9ed4bb1
huzhenli <huzhenli@eefung.com>
2018-07-23 16:32:08 +0800

    返回值中默认值的修改

--- portal/modules/crm/server/dto/contract.js
+++ portal/modules/crm/server/dto/contract.js
@@ -11,8 +11,8 @@ exports.toRestObject = (list) => {
             customer_name: item.customer_name || '', // 客户名称
             start_time: item.start_time || '', // 有效期的开始时间
             end_time: item.end_time || '', // 有效期的结束时间
-            contract_amount: item.contract_amount || '', // 合同额
-            gross_profit: item.gross_profit || '', // 毛利
+            contract_amount: item.contract_amount || 0, // 合同额
+            gross_profit: item.gross_profit || 0, // 毛利
             products: item.products || [], // 产品信息
             stage: item.stage || '', // 合同状态（待审、审核、归档、报废）
             remarks: item.remarks || '', // 合同备注
