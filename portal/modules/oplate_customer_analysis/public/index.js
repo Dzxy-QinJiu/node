bb140f1401892c2fc5f04fbfbe64de9b428c5bb4
yubin <yubin@eefung.com>
2018-08-03 10:18:35 +0800

    客户分析页增加“试用合格客户数统计”

--- portal/modules/oplate_customer_analysis/public/index.js
+++ portal/modules/oplate_customer_analysis/public/index.js
@@ -14,6 +14,7 @@ const AnalysisMenu = require('CMP_DIR/analysis_menu');
 const userData = require('PUB_DIR/sources/user-data');
 const emitters = require('PUB_DIR/sources/utils/emitters');
 const querystring = require('querystring');
+const history = require('PUB_DIR/sources/history');
 
 //从 unknown 到 未知 的对应关系对象
 const unknownObj = { name: Intl.get('user.unknown', '未知'), key: 'unknown' };
@@ -117,9 +118,154 @@ var OPLATE_CUSTOMER_ANALYSIS = React.createClass({
         return processOrderStageData(this.state.stageList, data);
     },
 
+    getDataType: function() {
+        if (hasPrivilege('GET_TEAM_LIST_ALL')) {
+            return 'all';
+        } else if (hasPrivilege('GET_TEAM_LIST_MYTEAM_WITH_SUBTEAMS')) {
+            return 'self';
+        } else {
+            return '';
+        }
+    },
+    //处理试用合格客户数统计数字点击事件
+    handleTrialQualifiedNumClick(customerIds) {
+        history.pushState({
+            from: 'sales_home',
+            trialQualifiedCustomerIds: customerIds
+        }, '/crm', {});
+    },
+    //试用合格客户数统计数字渲染函数
+    trialQualifiedNumRender(customerIdsField, text, record) {
+        const customerIds = record[customerIdsField];
+
+        if (customerIds) {
+            return (
+                <span onClick={this.handleTrialQualifiedNumClick.bind(this, customerIds)} style={{cursor: 'pointer'}}>
+                    {text}
+                </span>
+            );
+        } else {
+            return (
+                <span>
+                    {text}
+                </span>
+            );
+        }
+    },
+    //获取试用合格客户数统计图表
+    getTrialQualifiedChart() {
+        const dataType = this.getDataType();
+
+        //统计列
+        const statisticsColumns = [{
+            dataIndex: 'last_month',
+            title: '上月',
+            width: '10%',
+            render: this.trialQualifiedNumRender.bind(this, 'last_month_customer_ids'),
+        }, {
+            dataIndex: 'this_month',
+            title: '本月',
+            width: '10%',
+            render: this.trialQualifiedNumRender.bind(this, 'this_month_customer_ids'),
+        }, {
+            dataIndex: 'this_month_new',
+            title: '本月新增',
+            width: '10%',
+            render: this.trialQualifiedNumRender.bind(this, 'this_month_new_customer_ids'),
+        }, {
+            dataIndex: 'this_month_lose',
+            title: '本月流失',
+            width: '10%',
+            render: this.trialQualifiedNumRender.bind(this, 'this_month_lose_customer_ids'),
+        }, {
+            dataIndex: 'this_month_back',
+            title: '本月回流',
+            width: '10%',
+            render: this.trialQualifiedNumRender.bind(this, 'this_month_back_customer_ids'),
+        }, {
+            dataIndex: 'this_month_add',
+            title: '本月比上月净增',
+            width: '15%',
+        }, {
+            dataIndex: 'highest',
+            title: '历史最高',
+            width: '10%',
+        }, {
+            dataIndex: 'this_month_add_highest',
+            title: '本月比历史最高净增',
+            width: '20%',
+        }];
+
+        //表格列
+        let columns = _.cloneDeep(statisticsColumns);
+        columns.unshift({
+            title: '团队',
+            width: '10%',
+            dataIndex: 'team_name',
+        });
+
+        let chart = {
+            title: '试用合格客户数统计',
+            url: `/rest/analysis/customer/v2/statistic/${dataType}/customer/qualify`,
+            argCallback: (arg) => {
+                let query = arg.query;
+
+                if (query && query.starttime && query.endtime) {
+                    query.start_time = query.starttime;
+                    query.end_time = query.endtime;
+                    delete query.starttime;
+                    delete query.endtime;
+                }
+            },
+            layout: {sm: 24},
+            processData: data => {
+                data = data.list || [];
+                _.each(data, dataItem => {
+                    _.each(statisticsColumns, column => {
+                        const key = column.dataIndex;
+                        const customerIds = _.get(dataItem, [key, 'customer_ids']);
+
+                        if (customerIds) {
+                            dataItem[key + '_customer_ids'] = customerIds.join(',');
+                        }
+
+                        dataItem[key] = dataItem[key].total;
+                    });
+                });
+
+                return data;
+            },
+        };
+
+        _.extend(chart, {
+            chartType: 'table',
+            option: {
+                columns,
+            },
+            processOption: (option, chartProps) => {
+                //从返回数据里获取一下销售昵称
+                const nickName = _.get(chartProps, 'data[0].nick_name');
+
+                //若存在销售昵称，说明返回的是销售列表
+                if (nickName) {
+                    //找到名称列
+                    let nameColumn = _.find(option.columns, column => column.dataIndex === 'team_name');
+
+                    if (nameColumn) {
+                        //将名称列的数据索引改为指向昵称字段
+                        nameColumn.dataIndex = 'nick_name';
+                        //将名称列的标题改为销售
+                        nameColumn.title = '销售';
+                    }
+                }
+            },
+        });
+
+        return chart;
+    },
     //获取图表定义
     getCharts() {
-        return [{
+        let charts = [{
             title: 'Tabs',
             url: '/rest/analysis/customer/v1/:auth_type/summary',
             chartType: 'tab',
@@ -497,6 +643,12 @@ var OPLATE_CUSTOMER_ANALYSIS = React.createClass({
                 value: '',
             }],
         }];
+
+        const trialQualifiedChart = this.getTrialQualifiedChart();
+
+        charts.push(trialQualifiedChart);
+
+        return charts;
     },
 
     getEmitters() {
