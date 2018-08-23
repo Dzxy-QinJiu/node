e1674f69865c0539883ff99c50d6a7f31084369d
zhshj <1062735818@qq.com>
2018-08-23 17:00:34 +0800

    删除没用代码

--- portal/modules/clue_customer/public/index.js
+++ portal/modules/clue_customer/public/index.js
@@ -82,8 +82,6 @@ const ClueCustomer = React.createClass({
         this.getClueList();
         this.getUserPhoneNumber();
         clueEmitter.on(clueEmitter.IMPORT_CLUE, this.onClueImport);
-        //系统内有弹窗时，点击弹框中的线索名称可以查看线索详情
-        // notificationEmitter.on(notificationEmitter.SHOW_CLUE_DETAIL, this.showClueDetailFromNotification);
     },
     showClueDetailOut: function(item) {
         rightPanelShow = true;
@@ -108,17 +106,10 @@ const ClueCustomer = React.createClass({
             previewList: list,
         });
     },
-    // showClueDetailFromNotification: function(clueObj) {
-    //     var clueId = clueObj.clueId;
-    //     rightPanelShow = true;
-    //     this.setState({rightPanelIsShow: true});
-    //     clueCustomerAction.setCurrentCustomer(clueId);
-    // },
     componentWillUnmount: function() {
         clueCustomerStore.unlisten(this.onStoreChange);
         this.hideRightPanel();
         clueEmitter.removeListener(clueEmitter.IMPORT_CLUE, this.onClueImport);
-        // notificationEmitter.removeListener(notificationEmitter.SHOW_CLUE_DETAIL, this.showClueDetailFromNotification);
     },
     onStoreChange: function() {
         this.setState(clueCustomerStore.getState());
