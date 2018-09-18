2382c07aa7d72679ec2fc6c27b2a5d6bbe1984b4
xuning <xuning02@eefung.com>
2018-09-18 09:43:58 +0800

    样式调整

--- portal/components/shear-content/index.js
+++ portal/components/shear-content/index.js
@@ -3,21 +3,21 @@
 * 版权所有 (c) 2015-2018 湖南蚁坊软件股份有限公司。保留所有权利。
 * Created by xuning on 2018 9.17
 */
-require("./index.less");
-import shear from "shear.js";
+require('./index.less');
+import shear from 'shear.js';
 var classNames = require('classnames');
 class ShearContent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             showDetail: false
-        }
+        };
 
     }
     componentDidMount() {
         const $rootDOM = $(ReactDOM.findDOMNode(this));
-        this.$contentDOM = $rootDOM.find(".cut-content");
-        console.log(this.$contentDOM)
+        this.$contentDOM = $rootDOM.find('.cut-content');
+        console.log(this.$contentDOM);
         if (this.$contentDOM.length) {
             //转换成dom对象
             this.handleShear(this.$contentDOM[0]);
@@ -28,7 +28,7 @@ class ShearContent extends React.Component {
         shear($dom, this.props.lineNumber,
             `<span class='append-btn-bar'>
         ...<span class="append-icon shear-expand-btn">展开</span>
-        </span>`)
+        </span>`);
     }
     showDetail(isShow) {
         this.setState({
@@ -36,7 +36,7 @@ class ShearContent extends React.Component {
         }, () => {
             // this.$contentDOM.find('.append-btn-bar').toggle();
 
-        })
+        });
     }
     componentWillReceiveProps() {
         if (this.$contentDOM.length) {
@@ -48,20 +48,20 @@ class ShearContent extends React.Component {
     render() {
         const hideCls = classNames('cut-content', {
             'hide': this.state.showDetail
-        })
+        });
         const showCls = classNames('cut-content', {
             'hide': !this.state.showDetail
-        })
+        });
         return (
             <span className="shear-content-container">
                 <div className={hideCls}>
                     {this.props.children}
                 </div>
                 <div className={showCls}>
-                    {this.props.children}<span onClick={this.showDetail.bind(this, false)} className="append-icon">收起</span>
+                    {this.props.children}<span onClick={this.showDetail.bind(this, false)} className="append-icon collapse-btn">收起</span>
                 </div>
             </span>
-        )
+        );
     }
 }
 ShearContent.defaultProps = {
@@ -69,9 +69,9 @@ ShearContent.defaultProps = {
     children: null,
     jsx: null
 };
-ShearContent.PropTypes = {
+ShearContent.propTypes = {
     lineNumber: PropTypes.number,
     children: PropTypes.object,
     jsx: PropTypes.element
-}
+};
 export default ShearContent;
