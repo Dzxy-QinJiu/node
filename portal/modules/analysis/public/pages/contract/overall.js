6ed0c4540376d0f791865bd94469ee54ab553824
yubin <yubin@eefung.com>
2018-09-28 10:45:12 +0800

    增加合同分析

---
+++ portal/modules/analysis/public/pages/contract/overall.js
@@ -0,0 +1,21 @@
+/**
+ * 总体分析
+ */
+
+import contractChart from '../../charts/contract';
+
+module.exports = {
+    title: '总体分析',
+    privileges: [
+        'CUSTOMER_ANALYSIS_COMMON',
+        'CUSTOMER_ANALYSIS_MANAGER',
+    ],
+    charts: getCharts()
+};
+
+function getCharts() {
+    return [
+        //合同分析统计表
+        contractChart.getContractChart(),
+    ];
+}
