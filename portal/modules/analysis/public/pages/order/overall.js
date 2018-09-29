995a1a8df371a23d1646cac32a579c5b8cdddd48
yubin <yubin@eefung.com>
2018-09-29 14:10:51 +0800

    订单分析页里的图表改为放到单独的文件中

--- portal/modules/analysis/public/pages/order/overall.js
+++ portal/modules/analysis/public/pages/order/overall.js
@@ -2,8 +2,7 @@
  * 总体分析
  */
 
-import { processOrderStageData } from 'PUB_DIR/sources/utils/analysis-util';
-import Store from '../../store';
+import orderChart from '../../charts/order';
 
 module.exports = {
     title: '总体分析',
@@ -16,13 +15,7 @@ module.exports = {
 };
 
 function getCharts() {
-    return [{
-        title: Intl.get('oplate_customer_analysis.11', '订单阶段统计'),
-        url: '/rest/analysis/customer/v1/:auth_type/:tab/stage',
-        chartType: 'horizontalStage',
-        layout: {sm: 24},
-        processData: (data) => {
-            return processOrderStageData(Store.stageList, data);
-        },
-    }];
+    return [
+        orderChart.getOrderStageChart(),
+    ];
 }
