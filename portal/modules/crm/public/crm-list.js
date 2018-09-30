8fd4851c68141d094daf44b6fd2ab006039d0d2a
wangliping <wangliping@eefung.com>
2018-09-30 21:47:23 +0800

    合并客户按钮的修改

--- portal/modules/crm/public/crm-list.js
+++ portal/modules/crm/public/crm-list.js
@@ -954,11 +954,11 @@ class Crm extends React.Component {
             return (<div className="top-btn-wrapper">
                 <PrivilegeChecker
                     check="CUSTOMER_MERGE_CUSTOMER"
-                    className={btnClass}
+                    className={isWebMini ? 'block handle-btn-mini' : 'block crm-merge-btn'}
                     title={isWebMini ? Intl.get('crm.0', '合并客户') : ''}
                     onClick={this.showMergePanel}
                 >
-                    {isWebMini ? <i className="iconfont icon-merge-btn" /> : Intl.get('crm.0', '合并客户')}
+                    {isWebMini ? <i className="iconfont icon-merge-btn" /> : <Button>{Intl.get('crm.0', '合并客户')}</Button>}
                 </PrivilegeChecker>
                 <PrivilegeChecker check="CUSTOMER_BATCH_OPERATE" className="batch-btn-wrapper">
                     <CrmBatchChange isWebMini={isWebMini}
