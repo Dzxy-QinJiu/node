36a91c9516ab6a3e7baca031c3dbc04bf4619514
yubin <yubin@eefung.com>
2018-10-09 16:20:11 +0800

    试用合格客户分析增加趋势、地域、行业等统计

--- portal/modules/analysis/public/pages/customer/trial-qualified.js
+++ portal/modules/analysis/public/pages/customer/trial-qualified.js
@@ -18,5 +18,13 @@ function getCharts() {
     return [
         //试用合格客户数统计
         customerChart.getCustomerTrialEffectiveChart(),
+        //趋势统计
+        customerChart.getCustomerTrendChart(),
+        //历史净增
+        customerChart.getCustomerTrendChart('total', '历史净增'),
+        //地域统计
+        customerChart.getCustomerZoneChart(),
+        //行业统计
+        customerChart.getCustomerIndustryChart(),
     ];
 }
