88b5106b3c350eab741c66c29d1ea2b5309890f7
yubin <yubin@eefung.com>
2018-10-18 14:11:55 +0800

    总体帐号分析改为在单应用时显示柱状图

--- portal/modules/analysis/public/charts/account/num.js
+++ portal/modules/analysis/public/charts/account/num.js
@@ -2,39 +2,60 @@
  * 账号数统计
  */
 
+import { USER_TYPES, USER_TYPES_WITH_TITLE } from '../../consts';
+import { ifNotSingleApp } from '../../utils';
+
 export function getAccountNumChart(type = 'total', title) {
     return {
         title: title || '账号数统计',
         url: `/rest/analysis/user/v1/:auth_type/${type}/summary`,
-        chartType: 'line',
+        chartType: 'bar',
         option: {
             legend: {
-                type: 'scroll',
-                pageIconSize: 10,
+                data: USER_TYPES,
             },
         },
         customOption: {
-            multi: true,
-            serieNameField: 'app_name',
-            serieNameValueMap: {
-                '': Intl.get('oplate.user.analysis.22', '综合'),
-            },
+            stack: true,
         },
-        generateCsvData: function(data) {
-            let csvData = [];
-            let thead = [Intl.get('common.app.name', '应用名称')];
-            let subData = data[0] && data[0].data;
-            if (!subData) return [];
+        csvOption: {
+            rowNames: USER_TYPES_WITH_TITLE,
+        },
+        overide: {
+            condition: {
+                callback: ifNotSingleApp
+            },
+            chartType: 'line',
+            option: {
+                legend: {
+                    type: 'scroll',
+                    pageIconSize: 10,
+                },
+            },
+            customOption: {
+                stack: false,
+                multi: true,
+                serieNameField: 'app_name',
+                serieNameValueMap: {
+                    '': Intl.get('oplate.user.analysis.22', '综合'),
+                },
+            },
+            generateCsvData: function(data) {
+                let csvData = [];
+                let thead = [Intl.get('common.app.name', '应用名称')];
+                let subData = data[0] && data[0].data;
+                if (!subData) return [];
 
-            thead = thead.concat(_.map(subData, 'name'));
-            csvData.push(thead);
-            _.each(data, dataItem => {
-                const appName = dataItem.app_name || Intl.get('oplate.user.analysis.22', '综合');
-                let tr = [appName];
-                tr = tr.concat(_.map(dataItem.data, 'value'));
-                csvData.push(tr);
-            });
-            return csvData;
+                thead = thead.concat(_.map(subData, 'name'));
+                csvData.push(thead);
+                _.each(data, dataItem => {
+                    const appName = dataItem.app_name || Intl.get('oplate.user.analysis.22', '综合');
+                    let tr = [appName];
+                    tr = tr.concat(_.map(dataItem.data, 'value'));
+                    csvData.push(tr);
+                });
+                return csvData;
+            },
         },
     };
 }
