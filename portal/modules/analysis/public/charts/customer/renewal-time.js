f8be6f67a9c8fb2882be937cb57ef6bf11d2a8d2
yubin <yubin@eefung.com>
2018-10-25 15:11:26 +0800

    客户续签时间统计图按设计进行调整

--- portal/modules/analysis/public/charts/customer/renewal-time.js
+++ portal/modules/analysis/public/charts/customer/renewal-time.js
@@ -5,7 +5,48 @@
 export function getRenewalCustomerTimeChart() {
     return {
         title: '客户续签时间统计',
+        layout: {sm: 24},
+        height: 'auto',
         url: '/rest/analysis/customer/label/:data_type/renewal/time',
-        chartType: 'bar',
+        chartType: 'table',
+        processData: data => {
+            return _.map(data, item => {
+                let processedItem = {
+                    time: moment(item.timestamp).format('YYYYMM'),
+                    due_num: item.total,
+                };
+
+                _.each(item.points, (point, index) => {
+                    processedItem['month' + index] = point.count;
+                });
+
+                return processedItem;
+            });
+        },
+        option: {
+            columns: (() => {
+                let columns = [
+                    {
+                        title: '时间',
+                        dataIndex: 'time',
+                    }, {
+                        title: '到期客户数',
+                        dataIndex: 'due_num',
+                    }, {
+                        title: '当月续签',
+                        dataIndex: 'month0',
+                    }
+                ];
+
+                for (let i = 1; i < 13; i++) {
+                    columns.push({
+                        title: `延期${i}个月`,
+                        dataIndex: `month${i}`
+                    });
+                }
+
+                return columns;
+            })(),
+        },
     };
 }
