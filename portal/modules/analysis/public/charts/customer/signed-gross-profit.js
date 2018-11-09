bd8217fee90a095297a90bb11341f40508b5b8b1
yubin <yubin@eefung.com>
2018-11-09 18:21:23 +0800

    修复表格错位问题

--- portal/modules/analysis/public/charts/customer/signed-gross-profit.js
+++ portal/modules/analysis/public/charts/customer/signed-gross-profit.js
@@ -12,18 +12,23 @@ export function getSignedCustomerGrossProfitChart() {
             columns: [{
                 title: '新签',
                 dataIndex: 'new_gross_profit',
+                width: '20%',
             }, {
                 title: '回流',
                 dataIndex: 'reflux_gross_profit',
+                width: '20%',
             }, {
                 title: '扩展',
                 dataIndex: 'expansion_gross_profit',
+                width: '20%',
             }, {
                 title: '流失',
                 dataIndex: 'churn_gross_profit',
+                width: '20%',
             }, {
                 title: '净增',
                 dataIndex: 'net_new_gross_profit',
+                width: '20%',
             }],
         },
     };
