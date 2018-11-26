0bd9357177a5b468bafa84cf740655ff67445a8f
yubin <yubin@eefung.com>
2018-11-26 16:25:59 +0800

    增加有效账号数统计

---
+++ portal/modules/analysis/public/charts/account/valid.js
@@ -0,0 +1,25 @@
+/**
+ * 有效账号数统计
+ */
+
+import { ifNotSingleApp } from '../../utils';
+
+export function getAccountValidChart(type = 'total') {
+    return {
+        title: '有效账号数统计',
+        url: '/rest/analysis/user/v3/:data_type/total/valid',
+        chartType: 'pie',
+        noShowCondition: {
+            callback: ifNotSingleApp
+        },
+        processData: data => {
+            return [{
+                name: '有效数',
+                value: data.valid,
+            }, {
+                name: '收费数',
+                value: data.formal,
+            }];
+        },
+    };
+}
