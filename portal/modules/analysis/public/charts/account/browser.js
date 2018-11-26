faa69ea9d38a9b35123c50d47b8a26ed1f3cf483
yubin <yubin@eefung.com>
2018-11-26 16:00:02 +0800

    修复注释错误

--- portal/modules/analysis/public/charts/account/browser.js
+++ portal/modules/analysis/public/charts/account/browser.js
@@ -1,10 +1,10 @@
 /**
- * 账号状态统计
+ * 浏览器统计
  */
 
 import { ifNotSingleApp } from '../../utils';
 
-export function getAccountBrowserChart(type = 'total', title) {
+export function getAccountBrowserChart(type = 'total') {
     return {
         title: Intl.get('oplate.user.analysis.browser', '浏览器统计'),
         url: '/rest/analysis/user/v3/:auth_type/browser',
