076552ff92719465e287a939779a6527cb330f84
liwenjun <liwenjun@eefung.com>
2018-12-04 17:29:16 +0800

    分析和报告调整导航

--- portal/modules/monthly-report/public/index.js
+++ portal/modules/monthly-report/public/index.js
@@ -1,20 +1,23 @@
 var React = require('react');
 require('./style.less');
-import { AntcAnalysis } from 'antc';
-import { Row, Col, Select, DatePicker} from 'antd';
-import { hasPrivilege } from 'CMP_DIR/privilege/checker';
+import {AntcAnalysis} from 'antc';
+import {Row, Col, Select, DatePicker} from 'antd';
+import {hasPrivilege} from 'CMP_DIR/privilege/checker';
 import ajax from 'ant-ajax';
 import commonMethodUtil from 'PUB_DIR/sources/utils/common-method-util';
-import { LEAVE_TYPES } from './consts';
+import {LEAVE_TYPES} from './consts';
 import {AntcAttendanceRemarks} from 'antc';
+
 const TopNav = require('CMP_DIR/top-nav');
 import ReportLeftMenu from 'CMP_DIR/report-left-menu';
+
 const Option = Select.Option;
 const MonthPicker = DatePicker.MonthPicker;
 const Emitters = require('PUB_DIR/sources/utils/emitters');
 const dateSelectorEmitter = Emitters.dateSelectorEmitter;
 const teamTreeEmitter = Emitters.teamTreeEmitter;
-import { storageUtil } from 'ant-utils';
+import {storageUtil} from 'ant-utils';
+
 const STORED_TEAM_KEY = 'monthly_report_selected_team';
 import {getMyTeamTreeAndFlattenList} from 'PUB_DIR/sources/utils/common-data-util';
 
@@ -29,16 +32,16 @@ class MonthlyReport extends React.Component {
     componentDidMount() {
         //让顶部栏上的报告菜单显示选中状态
         $('.analysis_report_ico a').addClass('active');
-
         this.getTeamList();
         this.getMemberList();
+        this.props.renderTopNavOperation && this.props.renderTopNavOperation(this.renderTopNavOperation());
     }
 
     getTeamList = () => {
         const reqData = commonMethodUtil.getParamByPrivilege();
         getMyTeamTreeAndFlattenList(data => {
             var result = data.teamList;
-            if(!data.errorMsg) {
+            if (!data.errorMsg) {
                 const storedTeam = storageUtil.local.get(STORED_TEAM_KEY);
                 const selectedTeam = storedTeam || _.get(result, '[0]');
                 this.setState({
@@ -63,7 +66,7 @@ class MonthlyReport extends React.Component {
 
     getAuthType = () => {
         let authType = 'user';//CALL_RECORD_VIEW_USER
-        
+
         if (hasPrivilege('CALL_RECORD_VIEW_MANAGER')) {
             authType = 'manager';
         }
@@ -412,6 +415,11 @@ class MonthlyReport extends React.Component {
 
         dateSelectorEmitter.emit(dateSelectorEmitter.SELECT_DATE, startTime, endTime);
     };
+    //渲染操作按钮区
+    renderTopNavOperation = () => {
+        const selectedTeamId = _.get(this.state.selectedTeam, 'group_id');
+        return this.renderFilter(selectedTeamId);
+    };
 
     render() {
         const selectedTeamId = _.get(this.state.selectedTeam, 'group_id');
@@ -419,14 +427,10 @@ class MonthlyReport extends React.Component {
 
         return (
             <div className="monthly-report" data-tracename='销售月报'>
-                <TopNav>
-                    <TopNav.MenuList/>
-                    {this.renderFilter(selectedTeamId)}
-                </TopNav>
                 <div className="monthly-report-content">
                     <Row>
                         <Col span={3}>
-                            <ReportLeftMenu />
+                            <ReportLeftMenu/>
                         </Col>
                         <Col span={21}>
                             {selectedTeamName ? (
