0264a120c5f576f5839aee1fddbb068afda57af2
liwenjun <liwenjun@eefung.com>
2018-12-08 00:00:30 +0800

    修改route-bundle的路径

--- portal/modules/sales-report/index.js
+++ portal/modules/sales-report/index.js
@@ -1,7 +1,7 @@
 /**
  * 销售报告
  */
-import Bundle from '../../public/sources/route-bundle';
+import Bundle from '../../public/sources/route/route-bundle';
 
 const SalesReportPage = (props) => (
     <Bundle load={() => import('./public')}>
