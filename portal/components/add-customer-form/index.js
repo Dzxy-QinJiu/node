692187735d434f7a4a735a1a9b91457c49db4c63
wangliping <wangliping@eefung.com>
2019-01-23 10:07:08 +0800

    电话弹屏中添加客户时，填写客户名时，电话会被清空，保存验证不起作用的问题

--- portal/components/add-customer-form/index.js
+++ portal/components/add-customer-form/index.js
@@ -163,17 +163,26 @@ class AddCustomerForm extends React.Component {
         };
         ajax(arg).then(result => {
             if (_.isEmpty(result)) return;
-            const state = this.state;
-            state.formData.address = result.address;
-            state.formData.location = result.location;
-            state.formData.province = result.pname;
-            state.formData.city = result.cityname;
-            state.formData.county = result.adname;
-            state.formData.province_code = result.pcode;
-            state.formData.city_code = result.citycode;
-            state.formData.county_code = result.adcode;
-            state.formData.contacts0_phone = result.tel;
-            this.setState({formData: state.formData});
+            let formData = this.state.formData;
+            //下面的数据都没有时，再用获取的默认数据，（以防自己先填写了下面的数据，再修改用户名时，直接给清空或替换掉的问题）
+            if (!formData.address) {
+                formData.address = result.address;
+            }
+            if (!formData.location) {
+                formData.location = result.location;
+            }
+            if (!formData.province) {
+                formData.province = result.pname;
+                formData.city = result.cityname;
+                formData.county = result.adname;
+                formData.province_code = result.pcode;
+                formData.city_code = result.citycode;
+                formData.county_code = result.adcode;
+            }
+            if (!formData.contacts0_phone) {
+                formData.contacts0_phone = result.tel;
+            }
+            this.setState({formData: formData});
         });
     };
     closeAddPanel = (e) => {
