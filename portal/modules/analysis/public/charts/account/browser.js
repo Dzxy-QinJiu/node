9b63dda39c59c3b20a63ea5bb7c6b7066ae9c565
yubin <yubin@eefung.com>
2019-01-28 15:58:55 +0800

    修复总帐号浏览器统计团队id、成员id参数错误

--- portal/modules/analysis/public/charts/account/browser.js
+++ portal/modules/analysis/public/charts/account/browser.js
@@ -2,12 +2,16 @@
  * 浏览器统计
  */
 
-import { ifNotSingleApp } from '../../utils';
+import { ifNotSingleApp, argCallbackTeamId, argCallbackMemberIdToSalesId } from '../../utils';
 
 export function getAccountBrowserChart(type = 'total') {
     return {
         title: Intl.get('oplate.user.analysis.browser', '浏览器统计'),
         url: '/rest/analysis/user/v3/:auth_type/browser',
+        argCallback: arg => {
+            argCallbackTeamId(arg);
+            argCallbackMemberIdToSalesId(arg);
+        },
         chartType: 'bar',
         noShowCondition: {
             callback: ifNotSingleApp
