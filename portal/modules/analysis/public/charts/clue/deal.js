fa8283a226858ee46d90b05f7bf70694f2848e31
yubin <yubin@eefung.com>
2019-01-29 14:40:42 +0800

    修复线索分析成交额及成交数统计成员id参数错误

--- portal/modules/analysis/public/charts/clue/deal.js
+++ portal/modules/analysis/public/charts/clue/deal.js
@@ -2,23 +2,16 @@
  * 线索成交额及成交数统计
  */
 
+import { argCallbackMemberIdToIds, argCallbackTimeToUnderlineTime } from '../../utils';
+
 export function getClueDealChart() {
     return {
         title: '线索成交额及成交数统计',
         chartType: 'line',
         url: '/rest/analysis/customer/v2/clue/:data_type/sign/contract/amount',
         argCallback: (arg) => {
-            const query = arg.query;
-
-            if (query) {
-                //starttime转成start_time，endtime转成end_time
-                if (query.starttime && query.endtime) {
-                    query.start_time = query.starttime;
-                    query.end_time = query.endtime;
-                    delete query.starttime;
-                    delete query.endtime;
-                }
-            }
+            argCallbackTimeToUnderlineTime(arg);
+            argCallbackMemberIdToIds(arg);
         },
         dataField: 'list',
         nameField: 'date_str',
