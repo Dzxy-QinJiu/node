a49dfc0a0abee3af6ad479e695f846177f8524ae
yubin <yubin@eefung.com>
2019-02-13 11:35:15 +0800

    修复签约客户团队分布、新签客户团队分布、流失客户团队分布在按销售筛选时没有隐藏的问题

--- portal/modules/analysis/public/charts/customer/loss-team.js
+++ portal/modules/analysis/public/charts/customer/loss-team.js
@@ -2,10 +2,15 @@
  * 流失客户团队分析
  */
 
+import { isSales } from '../../utils';
+
 export function getLossCustomerTeamChart() {
     return {
         title: '流失客户团队分析',
         chartType: 'bar',
         url: '/rest/analysis/customer/label/:data_type/churn/team',
+        noShowCondition: {
+            callback: () => isSales()
+        },
     };
 }
--- portal/modules/analysis/public/charts/customer/signed-total-team.js
+++ portal/modules/analysis/public/charts/customer/signed-total-team.js
@@ -2,10 +2,15 @@
  * 签约客户团队分布
  */
 
+import { isSales } from '../../utils';
+
 export function getSignedCustomerTotalTeamChart() {
     return {
         title: '签约客户团队分布',
         url: '/rest/analysis/customer/label/:data_type/sign/total/team',
         chartType: 'bar',
+        noShowCondition: {
+            callback: () => isSales()
+        },
     };
 }
