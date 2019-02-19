1a89ad71a2bcd681f5478ef2eab7a6965b5c36d1
yubin <yubin@eefung.com>
2019-02-19 17:30:23 +0800

    修改线索有效性统计接口

--- portal/modules/analysis/public/charts/clue/availability.js
+++ portal/modules/analysis/public/charts/clue/availability.js
@@ -9,35 +9,7 @@ export function getAvailabilityChart() {
     return {
         title: Intl.get('clue.analysis.avalibility.chart', '有效性统计'),
         chartType: 'pie',
-        url: '/rest/clue/v1/:data_type/statistical/availability/1000/1',
-        reqType: 'post',
-        conditions: [{
-            type: 'data',
-            value: 'source_time',
-            callback: (data, name, value) => {
-                _.set(data, 'rang_params[0].name', value);
-            }
-        }, {
-            type: 'data',
-            value: 'time',
-            callback: (data, name, value) => {
-                _.set(data, 'rang_params[0].type', value);
-            }
-        }, {
-            name: 'starttime',
-            value: initialTime.start,
-            type: 'data',
-            callback: (data, name, value) => {
-                _.set(data, 'rang_params[0].from', value);
-            }
-        }, {
-            name: 'endtime',
-            value: initialTime.end,
-            type: 'data',
-            callback: (data, name, value) => {
-                _.set(data, 'rang_params[0].to', value);
-            }
-        }],
+        url: '/rest/analysis/customer/v2/clue/:data_type/statistical/field/availability',
         dataField: 'result',
         processData: processClueStatisticsData.bind(this, true),
         option: {
