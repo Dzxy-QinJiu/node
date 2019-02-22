39a16cf17b869d53ec0cc412c4b399906230b94d
yubin <yubin@eefung.com>
2019-02-22 09:33:26 +0800

    转出客户明细改用新接口

--- portal/modules/analysis/public/charts/customer/transfer.js
+++ portal/modules/analysis/public/charts/customer/transfer.js
@@ -7,35 +7,7 @@ import { initialTime } from '../../consts';
 export function getCustomerTransferChart() {
     return {
         title: '转出客户明细',
-        url: '/rest/customer/v2/customer/transfer/record/1000/time/descend',
-        reqType: 'post',
-        conditions: [{
-            type: 'data',
-            value: 'time',
-            callback: (data, name, value) => {
-                _.set(data, 'rang_params[0].name', value);
-            }
-        }, {
-            type: 'data',
-            value: 'time',
-            callback: (data, name, value) => {
-                _.set(data, 'rang_params[0].type', value);
-            }
-        }, {
-            name: 'starttime',
-            value: initialTime.start,
-            type: 'data',
-            callback: (data, name, value) => {
-                _.set(data, 'rang_params[0].from', value);
-            }
-        }, {
-            name: 'endtime',
-            value: initialTime.end,
-            type: 'data',
-            callback: (data, name, value) => {
-                _.set(data, 'rang_params[0].to', value);
-            }
-        }],
+        url: '/rest/analysis/customer/v2/all/transfer/record/1000/time/descend',
         dataField: 'result',
         chartType: 'table',
         layout: {
