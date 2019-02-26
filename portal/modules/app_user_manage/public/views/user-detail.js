6ddee92cd5327fbe9d441fa2d0459510954d4f39
huzhenli <huzhenli@eefung.com>
2019-02-26 13:26:30 +0800

    打开用户详情操作记录，左侧列表不能操作的问题修改

--- portal/modules/app_user_manage/public/views/user-detail.js
+++ portal/modules/app_user_manage/public/views/user-detail.js
@@ -376,7 +376,7 @@ class UserDetail extends React.Component {
                     <StatusWrapper
                     >
                         {
-                            !userInfo.errorMsg ? <div className="basic-info-contianer" data-trace="客户基本信息">
+                            !userInfo.errorMsg ? <div className="basic-info-contianer" data-trace="用户基本信息">
                                 <div className="basic-info-title-block clearfix">
                                     <div className="basic-info-name">
                                         <span className="basic-name-text" title={_.get(userInfo, 'data.user_name')}>{_.get(userInfo, 'data.user_name')}</span>
@@ -400,6 +400,7 @@ class UserDetail extends React.Component {
                                                             ref={ref => this.passwordRef = ref}
                                                             displayType="edit"
                                                             user_id={_.get(userInfo, 'data.user_id')}
+                                                            value=''
                                                             field="password"
                                                             type="password"
                                                             hideButtonBlock={true}
