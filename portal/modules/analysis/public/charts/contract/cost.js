2098d632fd79790229e623fa38ec43ddc18e89c9
yubin <yubin@eefung.com>
2019-03-01 09:13:55 +0800

    团队或个人费用统计标题加单位

--- portal/modules/analysis/public/charts/contract/cost.js
+++ portal/modules/analysis/public/charts/contract/cost.js
@@ -6,7 +6,7 @@ import { argCallbackUnderlineTimeToTime, argCallbackTeamIdsToTeamId, argCallback
 
 export function getCostChart() {
     return {
-        title: '团队或个人费用统计',
+        title: '团队或个人费用统计' + '(' + Intl.get('contract.160', '单位') + ': ' + Intl.get('contract.155', '元') + ')',
         url: '/rest/analysis/contract/contract/cost/team/amount',
         argCallback: arg => {
             argCallbackUnderlineTimeToTime(arg);
