0a71bf7bc8cdfbdb86dcf46b7110fa67c8a94631
zhshj <1062735818@qq.com>
2019-03-05 18:13:40 +0800

    代码走查问题修改

--- portal/public/sources/utils/common-method-util.js
+++ portal/public/sources/utils/common-method-util.js
@@ -664,7 +664,3 @@ exports.hasCalloutPrivilege = () => {
     let callClient = getCallClient();
     return callClient && callClient.isInited();
 };
-//文件大小的校验规则
-exports.REGFILESSIZERULES = () => {
-
-};
