1c2627e8d3e785e4dda60ef57a5b0cf2c1334d78
yubin <yubin@eefung.com>
2019-03-08 10:12:11 +0800

    修复线索阶段统计不能按团队、成员筛选的问题

--- portal/modules/analysis/public/charts/clue/stage.js
+++ portal/modules/analysis/public/charts/clue/stage.js
@@ -2,40 +2,19 @@
  * 线索阶段统计
  */
 
-import { initialTime } from '../../consts';
-import { argCallbackMemberIdsToMemberId } from '../../utils';
 import Store from '../../store';
 
 export function getStageChart() {
     return {
         title: Intl.get('clue.stage.statics', '线索阶段统计'),
         chartType: 'funnel',
-        url: '/rest/clue/v1/:data_type/statistical/customer_label/1000/1',
-        argCallback: arg => {
-            argCallbackMemberIdsToMemberId(arg);
-
-            _.set(arg, 'data.rang_params[0]', {
-                name: 'source_time',
-                type: 'time',
-                from: _.get(arg, 'query.start_time'),
-                to: _.get(arg, 'query.end_time')
-            });
-        },
-        reqType: 'post',
+        url: '/rest/analysis/customer/v2/clue/:data_type/statistical/field/customer_label',
         conditions: [{
             name: 'access_channel',
             value: '',
-            type: 'data',
-            callback: (data, name, value) => {
-                _.set(data, 'query[' + name + ']', value);
-            }
         }, {
             name: 'clue_source',
             value: '',
-            type: 'data',
-            callback: (data, name, value) => {
-                _.set(data, 'query[' + name + ']', value);
-            }
         }],
         dataField: 'result',
         processData: data => {
@@ -90,58 +69,4 @@ export function getStageChart() {
             }],
         },
     };
-}
-
-function processClueStaticsStageData(data) {
-    const customerStages = [
-        {
-            tagName: Intl.get('sales.stage.intention', '意向'),
-            tagValue: 'intention',
-        },
-        {
-            tagName: Intl.get('common.trial', '试用'),
-            tagValue: 'trial',
-        },
-        {
-            tagName: Intl.get('common.trial.qualified', '试用合格'),
-            tagValue: 'qualified',
-        },
-        {
-            tagName: Intl.get('sales.stage.signed', '签约'),
-            tagValue: 'signed',
-        }
-    ];
-
-    let processedData = [];
-    let prevStageValue;
-    customerStages.forEach(stage => {
-        let stageValue = 0;
-        _.forEach(data, (item) => {
-            _.forEach(item, (value, key) => {
-                if (key === stage.tagName) {
-                    stageValue = value;
-                }
-            });
-        });
-        if (stageValue) {
-            //保留原始值，用于在图表上显示
-            const showValue = stageValue;
-
-            // 如果下一阶段的值比上一阶段的值大，则将下一阶段的值变得比上一阶段的值小，以便能正确排序
-            if (prevStageValue && stageValue > prevStageValue) {
-                stageValue = prevStageValue * 0.8;
-            }
-
-            //将暂存的上一阶段的值更新为当前阶段的值，以供下一循环中使用
-            prevStageValue = stageValue;
-
-            processedData.push({
-                name: stage.tagName,
-                value: stageValue,
-                showValue,
-            });
-        }
-    });
-
-    return processedData;
 }
