33637bc17523f7adefeb4a6ce6d1846cb13eb70c
yubin <yubin@eefung.com>
2019-03-18 14:02:31 +0800

    修复客户阶段统计导出数据不对及在个人报告页面上报数据获取失败错误的问题

--- portal/modules/analysis/public/charts/customer/stage.js
+++ portal/modules/analysis/public/charts/customer/stage.js
@@ -3,6 +3,7 @@
  */
 
 import { processCustomerStageData } from 'PUB_DIR/sources/utils/analysis-util';
+import { argCallbackMemberIdToMemberIds, argCallbackUnderlineTimeToTime } from '../../utils';
 
 export function getCustomerStageChart(paramObj = {}) {
     return {
@@ -10,24 +11,39 @@ export function getCustomerStageChart(paramObj = {}) {
         url: '/rest/analysis/customer/stage/label/:auth_type/summary',
         chartType: 'funnel',
         argCallback: (arg) => {
-            if (arg.query) {
-                arg.query.starttime = 0;
+            //在个人报告里调用时会传member_id，需要改成member_ids
+            argCallbackMemberIdToMemberIds(arg);
 
-                //在个人报告里调用时会传member_id，需要改成member_ids
-                if (arg.query.member_id) {
-                    arg.query.member_ids = arg.query.member_id;
-                    delete arg.query.member_id;
-                }
-            }
-
+            //如果有页面级的统一处理，用统一处理
             if (_.isFunction(paramObj.argCallback)) {
                 paramObj.argCallback(arg);
+            //否则单独处理
+            } else {
+                argCallbackUnderlineTimeToTime(arg);
+            }
+
+            //这个统计是总体累计值，所以要把开始时间置为0
+            if (arg.query) {
+                arg.query.starttime = 0;
+            }
+
+            //app_id为必填参数，若参数中没有，需要设个默认的
+            if (!_.get(arg, 'query.app_id')) {
+                _.set(arg, 'query.app_id', 'all');
+            }
+
+            //按成员查询时，若未传返回类型参数，需要设个默认的，默认按成员返回
+            if (_.has(arg, 'query.member_ids') && !_.has(arg, 'query.statistics_type')) {
+                _.set(arg, 'query.statistics_type', 'user');
             }
         },
         processData: processCustomerStageData,
         customOption: {
             valueField: 'showValue',
             minSize: '5%',
+        },
+        csvOption: {
+            valueField: 'showValue',
         },
     };
 }
