c14cc1b6ab8ef4f3bc93c9aa1fb523c0ce1a14a7
wangliping <wangliping@eefung.com>
2019-04-11 21:48:23 +0800

    普通销售电话统计返回结果取值写错了的问题

--- portal/modules/common_sales_home_page/server/dto/salesObj.js
+++ portal/modules/common_sales_home_page/server/dto/salesObj.js
@@ -8,7 +8,7 @@ exports.toFrontSalesPhone = function(data) {
     if (data && _.isObject(data)) {
         salesPhone.salesRole = data.type;
         salesPhone.salesPhoneList = [];
-        if (_.isArray(data.resutl) && data.result.length > 0) {
+        if (_.isArray(data.result) && data.result.length > 0) {
             salesPhone.salesPhoneList = data.result.map(function(salesObj) {
                 return {
                     totalTime: salesObj.total_time || 0,//总时长
