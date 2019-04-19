8773a124dae7d3cea97349aa347d00818c6d12c2
yubin <yubin@eefung.com>
2019-04-19 12:20:44 +0800

    合并同名团队列

--- portal/modules/analysis/public/charts/chance/deal-detail.js
+++ portal/modules/analysis/public/charts/chance/deal-detail.js
@@ -8,12 +8,34 @@ export function getChanceDealDetailChart() {
         chartType: 'table',
         url: '/rest/analysis/customer/v2/sales_opportunity/:data_type/apply/deal/detail',
         dataField: 'list',
+        processData: data => {
+            let processedData = [];
+
+            let groupedData = _.groupBy(data, 'apply_team_name');
+
+            _.each(groupedData, rows => {
+                _.first(rows).rowSpan = rows.length;
+                processedData = processedData.concat(rows);
+            });
+
+            return processedData;
+        },
         option: {
             columns: [
                 {
                     title: '销售团队',
                     dataIndex: 'apply_team_name',
                     width: '10%',
+                    render: (value, row) => {
+                        let obj = {
+                            children: value,
+                            props: {
+                                rowSpan: row.rowSpan || 0
+                            }
+                        };
+
+                        return obj;
+                    }
                 }, {
                     title: '销售经理',
                     dataIndex: 'apply_nick_name',
