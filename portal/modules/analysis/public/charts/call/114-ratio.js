438811589242c70b8c5792b49bff211d250437cc
yubin <yubin@eefung.com>
2019-05-23 10:42:03 +0800

    114占比统计换为新接口

--- portal/modules/analysis/public/charts/call/114-ratio.js
+++ portal/modules/analysis/public/charts/call/114-ratio.js
@@ -6,24 +6,16 @@ export function getCall114RatioChart() {
     return {
         title: '114占比统计',
         chartType: 'bar',
-        url: '/rest/callrecord/v2/callrecord/term/:start_time/:end_time',
-        reqType: 'post',
-        argCallback: arg => {
-            let query = arg.query;
-            let params = arg.params;
-
-            if (query && params) {
-                query.filter_phone = false,
-                params.start_time = query.start_time;
-                params.end_time = query.end_time;
-                delete query.start_time;
-                delete query.end_time;
-            }
-        },
+        url: '/rest/analysis/callrecord/v1/callrecord/term/invailid',
+        conditions: [{
+            name: 'filter_phone',
+            value: false 
+        }, {
+            name: 'filter_invalid_phone',
+            value: true, 
+        }],
         processData: (data) => {
-            const list = _.get(data, 'list');
-
-            return _.map(list, item => {
+            return _.map(data, item => {
                 return {
                     name: item.sales_team,
                     value: item.rate
