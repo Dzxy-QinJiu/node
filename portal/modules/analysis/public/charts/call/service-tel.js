6563e87bb5a2540d6ab1627d5b402b32c03aa467
yubin <yubin@eefung.com>
2019-05-23 10:49:16 +0800

    客服电话统计换为新接口

--- portal/modules/analysis/public/charts/call/service-tel.js
+++ portal/modules/analysis/public/charts/call/service-tel.js
@@ -2,36 +2,20 @@
  * 客服电话统计
  */
 
-import Store from '../../store';
-
 export function getCallServiceTelChart() {
-    let url = '/rest/callrecord/v2/callrecord/term/:start_time/:end_time';
-
-    if (Store.teamMemberFilterType === 'member') {
-        url = url.replace('term', 'term/user');
-    }
-
     return {
         title: '客服电话统计',
         chartType: 'bar',
-        url,
-        reqType: 'post',
-        argCallback: arg => {
-            let query = arg.query;
-            let params = arg.params;
-
-            if (query && params) {
-                query.filter_invalid_phone = false,
-                params.start_time = query.start_time;
-                params.end_time = query.end_time;
-                delete query.start_time;
-                delete query.end_time;
-            }
-        },
+        url: '/rest/analysis/callrecord/v1/callrecord/term/invailid',
+        conditions: [{
+            name: 'filter_phone',
+            value: true
+        }, {
+            name: 'filter_invalid_phone',
+            value: false 
+        }],
         processData: (data) => {
-            const list = _.get(data, 'list');
-
-            return _.map(list, item => {
+            return _.map(data, item => {
                 return {
                     name: item.sales_team,
                     value: item.invalid_docs
