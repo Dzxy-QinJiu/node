e847c4a6ae13f2b6b6b2d52a3374ce30875ce0d6
huzhenli <huzhenli@eefung.com>
2019-06-05 17:26:33 +0800

    导入用户，更改为excel格式

--- portal/modules/app_user_manage/public/index.js
+++ portal/modules/app_user_manage/public/index.js
@@ -46,7 +46,7 @@ import NoDataIntro from 'CMP_DIR/no-data-intro';
 import IntegrateConfigView from './views/integrate-config/index';
 import TopNav from 'CMP_DIR/top-nav';
 import ImportUser from './views/import';
-import {CSV_FILES_TYPE_RULES} from 'PUB_DIR/sources/utils/consts';
+import {XLS_FILES_TYPE_RULES} from 'PUB_DIR/sources/utils/consts';
 
 /*用户管理界面外层容器*/
 class AppUserManage extends React.Component {
@@ -988,7 +988,7 @@ class AppUserManage extends React.Component {
                     closeTemplatePanel={this.closeImportUserRightPanel}
                     onItemListImport={this.onUserImport}
                     doImportAjax={this.doImportAjax}
-                    regRules={CSV_FILES_TYPE_RULES}
+                    regRules={XLS_FILES_TYPE_RULES}
                     importFileTips={Intl.get('user.import.user.toplimit', '每次导入上限为300个用户')}
                 />
             </div>
