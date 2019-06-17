c563037c21d12f35387aeecda0a1bee1189f7a47
yubin <yubin@eefung.com>
2019-06-17 10:36:09 +0800

    代码优化

--- portal/modules/clue_customer/public/views/clue-to-customer-panel.js
+++ portal/modules/clue_customer/public/views/clue-to-customer-panel.js
@@ -10,6 +10,13 @@ import { RightPanel } from 'CMP_DIR/rightPanel';
 //联系人表单
 const ContactForm = require('MOD_DIR/crm/public/views/contacts/contact-form');
 const noop = function() {};
+//视图类型
+const VIEW_TYPE = {
+    //相似客户列表视图
+    CUSTOMER_LIST: 'customer_list',
+    //合并客户视图
+    CUSTOMER_MERGE: 'customer_merge'
+};
 
 class ClueToCustomerPanel extends React.Component {
     static defaultProps = {
@@ -37,22 +44,14 @@ class ClueToCustomerPanel extends React.Component {
         super(props);
 
         this.state = {
-            //合并到客户的操作区块是否显示
-            isMergeCustomerBlockShow: false,
-            //要合并到的客户
-            toMergeCustomer: {},
-            //合并后的客户
-            mergedCustomer: {},
-            //要操作的联系人的索引
-            opContactIndex: -1,
-            //要操作的电话的索引
-            opPhoneIndex: -1,
-            //要操作的电话
-            opPhone: '',
-            //要修改的联系人
-            contactsToModify: [],
-            //要添加的联系人
-            contactsToAdd: [],
+            //视图类型
+            viewType: VIEW_TYPE.CUSTOMER_LIST,
+            //当前操作的客户的id
+            customerId: '',
+            //当前操作的客户的名称
+            customerName: '',
+            //当前操作的客户的联系人列表
+            customerContacts: [],
         };
     }
 
@@ -63,37 +62,13 @@ class ClueToCustomerPanel extends React.Component {
         })
             .done(result => {
                 this.setState({
-                    toMergeCustomer: customer,
+                    customerId: customer.id,
+                    customerName: customer.name,
+                    customerContacts: result
                 }, this.setMergedCustomer);
             })
             .fail(err => {
             });
-    }
-
-    //替换联系人名称按钮点击事件
-    onReplaceContactNameClick = (contactIndex, replaceName) => {
-        let mergedCustomer = _.cloneDeep(this.state.mergedCustomer);
-        let contactsToModify = _.cloneDeep(this.state.contactsToModify);
-        let contact = mergedCustomer.contacts[contactIndex];
-
-        //替换联系人名称
-        contact.name = replaceName;
-        contact.customer_id = mergedCustomer.id;
-        contact.modifyField = 'name';
-        delete contact.replaceName;
-        contactsToModify.push(contact);
-
-        this.setState({
-            mergedCustomer,
-            contactsToModify
-        });
-    }
-
-    //隐藏合并客户操作区块
-    hideMergeCustomerBlock = () => {
-        this.setState({
-            isMergeCustomerBlockShow: false,
-        });
     }
 
     //设置已合并客户
@@ -104,49 +79,43 @@ class ClueToCustomerPanel extends React.Component {
         //没有当前线索时直接返回
         if (_.isEmpty(clue)) return;
 
-        //合并后的客户
-        let mergedCustomer = _.cloneDeep(this.state.toMergeCustomer);
-        let contactsToModify = _.cloneDeep(this.state.contactsToModify);
-        let contactsToAdd = _.cloneDeep(this.state.contactsToAdd);
+        //线索联系人列表
+        const clueContacts = clue.contacts;
+        //客户联系人列表
+        let customerContacts = _.cloneDeep(this.state.customerContacts);
 
-        //遍历客户联系人
-        _.each(mergedCustomer.contacts, customerContact => {
-            //遍历线索联系人
-            _.some(clue.contacts, clueContact => {
+        //遍历客户联系人列表
+        _.each(customerContacts, customerContact => {
+            //遍历线索联系人列表
+            _.some(clueContacts, clueContact => {
                 //客户联系人电话和线索联系人电话的合集
                 const allPhone = _.concat(customerContact.phone, clueContact.phone);
                 //去重后的电话合集
                 const uniqPhone = _.uniq(allPhone);
+                //电话是否重复
+                //如果去重后电话总数少了，说明有重复的电话
+                const isPhoneDup = allPhone.length > uniqPhone.length;
 
-                //如果存在同名联系人，说明联系人重复
-                if (clueContact.name === customerContact.name) {
+                //联系人名是否重复
+                //如果线索和客户存在同名联系人，说明联系人重复
+                const isContactNameDup = clueContact.name === customerContact.name;
+
+                //联系人名重复或电话重复都认为是联系人重复
+                const isContactDup = isContactNameDup || isPhoneDup;
+
+                //如果联系人重复
+                if (isContactDup) {
                     //将客户联系人的电话设置为去重后的电话合集
                     customerContact.phone = uniqPhone;
                     //将该客户联系人标记为需要更新电话
-                    customerContact.modifyField = 'phone';
-                    //设置客户id
-                    customerContact.customer_id = mergedCustomer.id;
-
-                    //加入需要更新的联系人列表
-                    contactsToModify.push(customerContact);
+                    customerContact.updateFields = ['phone'];
 
                     //将该线索联系人标记为重复联系人
                     clueContact.isDup = true;
 
-                    //中止遍历
-                    return true;
-                //如果电话重复
-                } else if (allPhone.length > uniqPhone.length) {
-                    //将客户联系人的电话设置为去重后的电话合集
-                    customerContact.phone = uniqPhone;
-                    //将该客户联系人标记为重复联系人
-                    customerContact.isDup = true;
-                    //将该线索联系人标记为重复联系人
-                    clueContact.isDup = true;
-                    
-                    //如果有重复电话的客户联系人和线索联系人的名字不相同
-                    if (customerContact.name !== clueContact.name) {
-                        //将线索联系人的名字设置为客户联系人的替换名字
+                    //如果电话重复且客户联系人和线索联系人的名字不相同
+                    if (isPhoneDup && customerContact.name !== clueContact.name) {
+                        //将客户联系人的替换名字设置为线索联系人的名字，以供用户选择
                         customerContact.replaceName = clueContact.name;
                     }
 
@@ -159,39 +128,59 @@ class ClueToCustomerPanel extends React.Component {
         //和客户联系人的名称及电话都不重复的线索联系人
         let noneDupClueContacts = _.filter(clue.contacts, clueContact => !clueContact.isDup);
 
+        //遍历不重复的线索联系人
         _.each(noneDupClueContacts, noneDupClueContact => {
             //添加客户id
-            noneDupClueContact.customer_id = mergedCustomer.id;
+            noneDupClueContact.customer_id = this.state.customerId;
             //标记为新联系人
             noneDupClueContact.isNew = true;
         });
 
         //将这些不重复的联系人合并到客户联系人
-        mergedCustomer.contacts = _.concat(mergedCustomer.contacts, noneDupClueContacts);
-
-        //将这些不重复的联系人合并到要添加的联系人列表
-        contactsToAdd = _.concat(contactsToAdd, noneDupClueContacts);
+        customerContacts = _.concat(customerContacts, noneDupClueContacts);
 
         this.setState({
-            mergedCustomer,
-            contactsToModify,
-            contactsToAdd,
-            isMergeCustomerBlockShow: true,
+            customerContacts,
+            viewType: VIEW_TYPE.CUSTOMER_MERGE
         });
+    }
+
+    //替换联系人名称按钮点击事件
+    onReplaceContactNameClick = (contactIndex, replaceName) => {
+        //客户联系人列表
+        let customerContacts = _.cloneDeep(this.state.customerContacts);
+
+        //要更新的联系人
+        let contact = customerContacts[contactIndex];
+
+        //替换联系人名称
+        contact.name = contact.replaceName;
+        //删除暂存的替换名称
+        delete contact.replaceName;
+        //在联系人需要更新的字段中加上名称
+        contact.updateFields.push('name');
+
+        this.setState({ customerContacts });
+    }
+
+    //设置视图类型
+    setViewType = viewType => {
+        this.setState({ viewType });
     }
 
     //合并到客户
     mergeToCustomer = () => {
+        const contacts = this.state.customerContacts;
         const clueId = this.props.clue.id;
-        const contactsToModify = this.state.contactsToModify;
-        const contactsToAdd = this.state.contactsToAdd;
 
-        if (!_.isEmpty(contactsToModify)) {
-            _.each(contactsToModify, contact => {
-                const type = contact.modifyField;
-                delete contact.modifyField;
+        _.each(contacts, contact => {
+            //如果没有需要更新的字段，直接返回
+            if (_.isEmpty(contact.updateFields)) return;
+
+            //遍历需要更新的字段
+            _.each(contact.updateFields, field => {
                 ajax.send({
-                    url: `/rest/customer/v3/contacts/property/${type}/lead?clue_id=${clueId}`,
+                    url: `/rest/customer/v3/contacts/property/${field}/lead?clue_id=${clueId}`,
                     type: 'put',
                     data: contact
                 })
@@ -202,70 +191,21 @@ class ClueToCustomerPanel extends React.Component {
                         this.setState({
                             isShowClueToCustomerPanel: false,
                             isShowAddCustomerPanel: true,
-                            existingCustomers: []
                         });
                         */
                     });
             });
-        }
+
+            delete contact.updateFields;
+        });
     }
 
-    //渲染添加新客户区块
-    renderAddCustomerBlock() {
-        const clue = this.props.clue;
-
-        return (
-            <div className="basic-info-block">
-                <Row>
-                    <Col span={4}>
-                        {Intl.get('crm.41', '客户名')}：
-                    </Col>
-                    <Col span={20}>
-                        {clue.name}
-                    </Col>
-                </Row>
-                {_.map(clue.contacts, contact => {
-                    return (
-                        <div>
-                            <Row>
-                                <Col span={4}>
-                                    {Intl.get('call.record.contacts', '联系人')}：
-                                </Col>
-                                <Col span={20}>
-                                    {contact.name}
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col span={4}>
-                                    {Intl.get('common.phone', '电话')}：
-                                </Col>
-                                <Col span={20}>
-                                    {_.map(contact.phone, phone => {
-                                        return <div>{phone}</div>;
-                                    })}
-                                </Col>
-                            </Row>
-                        </div>
-                    );
-                })}
-                <Row>
-                    <Col span={4}>
-                        {Intl.get('crm.6', '负责人')}：
-                    </Col>
-                    <Col span={20}>
-                        {clue.user_name}
-                    </Col>
-                </Row>
-            </div>
-        );
-    }
-
-    //渲染已存在客户区块
-    renderExistingCustomersBlock() {
+    //渲染客户列表
+    renderCustomerList() {
         const existingCustomers = this.props.existingCustomers;
 
         return (
-            <div className="exists-customer-block">
+            <div className="customer-list">
                 <div className="title">
                     <b>{Intl.get('common.has.similar.customers', '有{count}个信息相似的客户', {count: existingCustomers.length})}</b>
                 </div>
@@ -343,7 +283,7 @@ class ClueToCustomerPanel extends React.Component {
     }
 
     //渲染联系人表单
-    renderContactForm(contact, contactIndex) {
+    renderContactForm(contact) {
         contact = {contact};
 
         return (
@@ -356,33 +296,31 @@ class ClueToCustomerPanel extends React.Component {
         );
     }
 
-    //渲染合并客户区块
-    renderMergeCustomerBlock() {
-        const customer = this.state.mergedCustomer;
-
+    //渲染客户合并
+    renderCustomerMerge() {
         return (
-            <div className="merge-customer-block">
+            <div className="customer-merge">
                 <div className="title">
                     <b>合并到此客户</b>
-                    <span className="go-back clickable" onClick={this.hideMergeCustomerBlock}>返回</span>
+                    <span className="go-back clickable" onClick={this.setViewType.bind(this, VIEW_TYPE.CUSTOMER_LIST)}>返回</span>
                 </div>
                 <Row>
                     <Col span={4}>
                         {Intl.get('crm.41', '客户名')}：
                     </Col>
                     <Col span={20}>
-                        {customer.name}
+                        {this.state.customerName}
                     </Col>
                 </Row>
-                {_.map(customer.contacts, (contact, contactIndex) => {
+                {_.map(this.state.customerContacts, (contact, contactIndex) => {
                     if (contact.isNew) {
-                        return this.renderContactForm(contact, contactIndex);
+                        return this.renderContactForm(contact);
                     } else {
                         return this.renderContact(contact, contactIndex);
                     }
                 })}
                 <div className="btn-block">
-                    <Button onClick={this.hideMergeCustomerBlock}>{Intl.get('common.cancel', '取消')}</Button>
+                    <Button onClick={this.setViewType.bind(this, VIEW_TYPE.CUSTOMER_LIST)}>{Intl.get('common.cancel', '取消')}</Button>
                     <Button type="primary" onClick={this.mergeToCustomer}>确认合并</Button>
                 </div>
             </div>
@@ -390,11 +328,6 @@ class ClueToCustomerPanel extends React.Component {
     }
 
     render() {
-        //相似客户区块是否显示
-        const isExistingCustomersBlockShow = this.props.existingCustomers.length && !this.state.isMergeCustomerBlockShow;
-        //合并客户区块是否显示
-        const isMergeCustomerBlockShow = this.state.isMergeCustomerBlockShow;
-
         return (
             <RightPanel
                 className="clue_customer_rightpanel clue-to-customer-panel"
@@ -404,8 +337,8 @@ class ClueToCustomerPanel extends React.Component {
                 <span className="iconfont icon-close clue-right-btn" onClick={this.props.hidePanel} data-tracename="关闭线索转客户面板"></span>
                 <div className="clue-detail-wrap">
                     <div className="panel-content">
-                        {isExistingCustomersBlockShow ? this.renderExistingCustomersBlock() : null}
-                        {isMergeCustomerBlockShow ? this.renderMergeCustomerBlock() : null}
+                        {this.state.viewType === VIEW_TYPE.CUSTOMER_LIST ? this.renderCustomerList() : null}
+                        {this.state.viewType === VIEW_TYPE.CUSTOMER_MERGE ? this.renderCustomerMerge() : null}
                     </div>
                 </div>
             </RightPanel>
