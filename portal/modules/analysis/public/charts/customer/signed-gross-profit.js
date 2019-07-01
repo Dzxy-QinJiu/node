b001a72e820fb61dfd0375890e65ef01a6e36ec0
yubin <yubin@eefung.com>
2019-07-01 16:51:32 +0800

    签约客户毛利分析的时间区间参数调整成和签约客户净增分析一致的

--- portal/modules/analysis/public/charts/customer/signed-gross-profit.js
+++ portal/modules/analysis/public/charts/customer/signed-gross-profit.js
@@ -7,7 +7,13 @@ export function getSignedCustomerGrossProfitChart(paramObj = {}) {
         title: '签约客户毛利分析(单位: 元)',
         chartType: 'table',
         url: '/rest/analysis/customer/label/:data_type/sign/customer',
-        argCallback: paramObj.argCallback,
+        argCallback: arg => {
+            paramObj.argCallback(arg);
+
+            let query = arg.query;
+            //用日期选择器上当前选择的时间区间作为查询的时间区间
+            query.interval = query.time_range;
+        },
         processData: data => [data],
         option: {
             columns: [{
