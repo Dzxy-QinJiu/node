190a751b4c4cd124de4ee274ea64d1f26f9de982
yubin <yubin@eefung.com>
2019-07-10 14:31:08 +0800

    帐号来源分类统计增加一其他项

--- portal/modules/analysis/public/charts/account/source.js
+++ portal/modules/analysis/public/charts/account/source.js
@@ -14,11 +14,14 @@ export function getAccountSourceChart(type = 'total', title = '') {
         },
         processData: data => {
             return [{
-                name: '线索转化',
+                name: Intl.get('common.clue.convert', '线索转化'),
                 value: data.lead,
             }, {
-                name: '销售开发',
+                name: Intl.get('common.sales.develop', '销售开发'),
                 value: data.sales,
+            }, {
+                name: Intl.get('crm.186', '其他'),
+                value: data.other,
             }];
         },
     };
