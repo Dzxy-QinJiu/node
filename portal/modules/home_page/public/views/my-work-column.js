c8e440457af527b4e267c6ff55a19d2199814d50
wangliping <wangliping@eefung.com>
2019-07-11 14:44:21 +0800

    去掉误加的 _

--- portal/modules/home_page/public/views/my-work-column.js
+++ portal/modules/home_page/public/views/my-work-column.js
@@ -87,7 +87,7 @@ class MyWorkColumn extends React.Component {
     //关闭、切换详情前，已完成工作的处理
     handleFinishedWork = () => {
         let handlingWork = this.state.handlingWork;
-        if (handlingWork && handlingWork.isFinished_) {
+        if (handlingWork && handlingWork.isFinished) {
             this.handleMyWork(this.state.handlingWork);
         }
     }
