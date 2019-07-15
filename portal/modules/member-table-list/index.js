d2dff8fdb38b46f7ef422bb7d834be3c39b604f5
huzhenli <huzhenli@eefung.com>
2019-07-15 10:48:57 +0800

    抽取公共的部分

---
+++ portal/modules/member-table-list/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by hzl on 2019/7/15.
+ */
+
+require('./index.less');
+import {AntcTable} from 'antc';
+
+class MemberTableList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+}
+
+MemberTableList;
