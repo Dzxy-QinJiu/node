61f54a870a2c389ac56a67b9ac874a9cbc972b9d
yubin <yubin@eefung.com>
2019-07-16 14:56:28 +0800

    线索转客户时将除电话外的其他联系方式也转过去

--- portal/modules/clue_customer/public/views/clue-to-customer-panel.js
+++ portal/modules/clue_customer/public/views/clue-to-customer-panel.js
@@ -21,6 +21,9 @@ const VIEW_TYPE = {
     //合并客户视图
     CUSTOMER_MERGE: 'customer_merge'
 };
+//联系方式种类
+const CONTACT_WAY_TYPES = ['phone', 'qq', 'weChat', 'email'];
+
 
 class ClueToCustomerPanel extends React.Component {
     static defaultProps = {
@@ -259,6 +262,39 @@ class ClueToCustomerPanel extends React.Component {
             });
     }
 
+    //获取联系方式对比结果
+    getContactWayComparisonResult(customerContact, clueContact) {
+        let result = {
+            //联系方式是否重复
+            isDup: false
+        };
+
+        //遍历联系方式类型
+        _.each(CONTACT_WAY_TYPES, type => {
+
+            //客户联系人中该联系方式和线索联系人中该联系方式的合集
+            const all = _.concat(customerContact[type], clueContact[type]);
+            //去重后的该联系方式合集
+            const uniqSet = _.uniq(all);
+            //该联系方式是否重复
+            //如果去重后该联系方式总数少了，说明该联系方式有重复
+            const isDup = all.length > uniqSet.length;
+
+            //任一联系方式有重复，都表明该联系人的联系方式有重复
+            if (isDup) result.isDup = true;
+
+            //该联系方式是否有不同
+            const isDiff = _.difference(customerContact.phone, clueContact.phone).length;
+
+            result[type] = {
+                isDiff,
+                uniqSet
+            };
+        });
+
+        return result;
+    }
+
     //设置已合并客户
     setMergedCustomer() {
         //当前线索
@@ -275,37 +311,47 @@ class ClueToCustomerPanel extends React.Component {
         _.each(customerContacts, customerContact => {
             //遍历线索联系人列表
             _.some(clueContacts, clueContact => {
-                //客户联系人电话和线索联系人电话的合集
-                const allPhone = _.concat(customerContact.phone, clueContact.phone);
-                //去重后的电话合集
-                const uniqPhone = _.uniq(allPhone);
-                //电话是否重复
-                //如果去重后电话总数少了，说明有重复的电话
-                const isPhoneDup = allPhone.length > uniqPhone.length;
+                //联系方式对比结果
+                const contactWayComparisonResult = this.getContactWayComparisonResult(customerContact, clueContact);
+
+                //联系方式是否重复
+                const isContactWayDup = contactWayComparisonResult.isDup;
 
                 //联系人名是否重复
                 //如果线索和客户存在同名联系人，说明联系人重复
                 const isContactNameDup = clueContact.name === customerContact.name;
 
-                //联系人名重复或电话重复都认为是联系人重复
-                const isContactDup = isContactNameDup || isPhoneDup;
+                //如果联系方式有重复但名字不重复
+                if (isContactWayDup && !isContactNameDup) {
+                    //将客户联系人的替换名字设置为线索联系人的名字，以供用户选择
+                    customerContact.replaceName = clueContact.name;
+                    isConfirmMergeBtnDisabled = true;
+                }
+
+                //遍历联系方式类型
+                _.each(CONTACT_WAY_TYPES, type => {
+                    //如果联系方式有不同
+                    if (contactWayComparisonResult[type].isDiff) {
+                        //标记联系方式需要更新
+                        if (_.isArray(customerContact.updateFields)) {
+                            customerContact.updateFields.push(type);
+                        } else {
+                            customerContact.updateFields = [type];
+                        }
+
+                        //将联系方式设为合并后的联系方式
+                        customerContact[type] = contactWayComparisonResult[type].uniqSet;
+                    }
+                });
+
+                //联系人是否重复
+                //联系人名重复或联系方式重复都认为是联系人重复
+                const isContactDup = isContactNameDup || isContactWayDup;
 
                 //如果联系人重复
                 if (isContactDup) {
-                    //将客户联系人的电话设置为去重后的电话合集
-                    customerContact.phone = uniqPhone;
-                    //将该客户联系人标记为需要更新电话
-                    customerContact.updateFields = ['phone'];
-
                     //将该线索联系人标记为重复联系人
                     clueContact.isDup = true;
-
-                    //如果电话重复且客户联系人和线索联系人的名字不相同
-                    if (isPhoneDup && customerContact.name !== clueContact.name) {
-                        //将客户联系人的替换名字设置为线索联系人的名字，以供用户选择
-                        customerContact.replaceName = clueContact.name;
-                        isConfirmMergeBtnDisabled = true;
-                    }
 
                     //中止遍历
                     return true;
@@ -399,9 +445,6 @@ class ClueToCustomerPanel extends React.Component {
         const changedContacts = _.filter(contacts, contact => contact.isNew || !_.isEmpty(contact.updateFields));
 
         if (!_.isEmpty(changedContacts)) {
-            //已经完成的ajax请求数
-            let ajaxDoneNum = 0;
-
             const promises = [];
 
             _.each(contacts, (contact, index) => {
@@ -417,12 +460,10 @@ class ClueToCustomerPanel extends React.Component {
                         contact.birthday = contact.birthday.valueOf();
                     }
 
-                    const fields = ['phone', 'qq', 'weChat', 'email'];
-
                     _.each(contact, (value, key) => {
                         const keyWithoutIndex = key.substr(0, key.length - 1);
 
-                        if (_.includes(fields, keyWithoutIndex)) {
+                        if (_.includes(CONTACT_WAY_TYPES, keyWithoutIndex)) {
                             if (!contact[keyWithoutIndex]) {
                                 contact[keyWithoutIndex] = [value];
                             } else {
@@ -448,7 +489,7 @@ class ClueToCustomerPanel extends React.Component {
                             url: `/rest/customer/v3/contacts/property/${field}/lead?clue_id=${clueId}`,
                             type: 'put',
                             data: contact
-                        }, `clueToCustomer${index}`);
+                        }, `clueToCustomer${index}${field}`);
 
                         promises.push(promise);
                     });
@@ -566,10 +607,16 @@ class ClueToCustomerPanel extends React.Component {
 
     //渲染联系人标题
     renderContactTitle(contact, contactIndex) {
+        let iconClassName = 'iconfont icon-contact-default is-default-contact';
+
+        if (contact.def_contancts === 'true') {
+            iconClassName += ' is-default-contact';
+        }
+
         return (
             <div className="contact-title">
                 <div className="contact-name">
-                    <span className="iconfont icon-contact-default is-default-contact"></span>
+                    <span className={iconClassName}></span>
                     {contact.name}
                 </div>
 
