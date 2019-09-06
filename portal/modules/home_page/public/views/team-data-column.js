58c23c7ad8ca7f11e5160d3d109ad94f8babeefb
tmq520 <1005275284@qq.com>
2019-09-06 10:07:41 +0800

    我的数据中电话量可以选择查看时间

--- portal/modules/home_page/public/views/team-data-column.js
+++ portal/modules/home_page/public/views/team-data-column.js
@@ -4,21 +4,52 @@
  * Created by wangliping on 2019/6/12.
  */
 import '../css/my-data-column.less';
-import {Progress, Tooltip} from 'antd';
+import {Progress, Tooltip, Select} from 'antd';
+const Option = Select.Option;
 import ColumnItem from './column-item';
 import myDataAjax from '../ajax';
 import DetailCard from 'CMP_DIR/detail-card';
+import Spinner from 'CMP_DIR/spinner';
 import userData from 'PUB_DIR/sources/user-data';
-import {getTodayTimeStamp, getThisWeekTimeStamp} from 'PUB_DIR/sources/utils/time-stamp-util';
+import {getTodayTimeStamp, getThisWeekTimeStamp, getLastWeekTimeStamp} from 'PUB_DIR/sources/utils/time-stamp-util';
 import TimeUtil from 'PUB_DIR/sources/utils/time-format-util';
+import classNames from 'classnames';
+
 const performance = {
     image_1: require('../images/performance_1.png'),
     image_2: require('../images/performance_2.png'),
     image_3: require('../images/performance_3.png')
 };
+const DATE_TYPE_KEYS = {
+    TODAY: 'today',
+    YESTERDAY: 'yesterday',
+    CURRENT_WEEK: 'week',
+    LAST_WEEK: 'lastWeek',
+};
+const DATE_TYPE_MAP = [
+    {
+        name: Intl.get('user.time.today', '今天'),
+        value: DATE_TYPE_KEYS.TODAY
+    },
+    {
+        name: Intl.get('user.time.yesterday', '昨天'),
+        value: DATE_TYPE_KEYS.YESTERDAY
+    },
+    {
+        name: Intl.get('common.current.week', '本周'),
+        value: DATE_TYPE_KEYS.CURRENT_WEEK
+    },
+    {
+        name: Intl.get('user.time.prev.week', '上周'),
+        value: DATE_TYPE_KEYS.LAST_WEEK
+    },
+];
+
+
 class TeamDataColumn extends React.Component {
     constructor(props) {
         super(props);
+        let type = this.getDefaultCallTimeType();
         this.state = {
             //业绩排名数据
             performanceData: {},
@@ -26,6 +57,10 @@ class TeamDataColumn extends React.Component {
             callTimeData: [],
             //本周联系客户总数
             contactCustomerCount: 0,
+            // 当前选择的时间类型
+            currentDateType: type,
+            callTimeLoading: false,
+            callTimeErrMsg: '',
         };
     }
 
@@ -51,31 +86,60 @@ class TeamDataColumn extends React.Component {
         });
     }
 
-    getCallTimeObj() {
-        let callTimeObj = {
-            startTime: 0,
-            endTime: moment().valueOf(),
-            callTimeDescr: ''
-        };
+    getDefaultCallTimeType() {
+        let type = DATE_TYPE_KEYS.TODAY;
         //销售
         if (this.isSalesRole()) {
             //普通销售
             if (userData.getUserData().isCommonSales) {
-                callTimeObj.callTimeDescr = Intl.get('user.time.today', '今天');
-                callTimeObj.startTime = getTodayTimeStamp().start_time;
+                type = DATE_TYPE_KEYS.TODAY;
             } else {//销售总监、主管
                 //昨天
-                callTimeObj.callTimeDescr = Intl.get('user.time.yesterday', '昨天');
-                callTimeObj.startTime = getTodayTimeStamp().start_time - oplateConsts.ONE_DAY_TIME_RANGE;
-                callTimeObj.endTime = getTodayTimeStamp().end_time - oplateConsts.ONE_DAY_TIME_RANGE;
+                type = DATE_TYPE_KEYS.YESTERDAY;
             }
         } else {//管理员、运营
             //本周
-            callTimeObj.callTimeDescr = Intl.get('common.current.week', '本周');
-            callTimeObj.startTime = getThisWeekTimeStamp().start_time;
+            type = DATE_TYPE_KEYS.CURRENT_WEEK;
+        }
+        return type;
+    }
+
+    getCallTimeObj() {
+        let curDateObj = _.find(DATE_TYPE_MAP, date => date.value === this.state.currentDateType);
+        let callTimeObj = {
+            startTime: 0,
+            endTime: moment().valueOf(),
+            callTimeDescr: curDateObj.name,
+            type: curDateObj.value
+        };
+
+        switch (this.state.currentDateType) {
+            case DATE_TYPE_KEYS.TODAY://今天
+                callTimeObj.startTime = getTodayTimeStamp().start_time;
+                break;
+            case DATE_TYPE_KEYS.YESTERDAY://昨天
+                callTimeObj.startTime = getTodayTimeStamp().start_time - oplateConsts.ONE_DAY_TIME_RANGE;
+                callTimeObj.endTime = getTodayTimeStamp().end_time - oplateConsts.ONE_DAY_TIME_RANGE;
+                break;
+            case DATE_TYPE_KEYS.CURRENT_WEEK://本周
+                callTimeObj.startTime = getThisWeekTimeStamp().start_time;
+                break;
+            case DATE_TYPE_KEYS.LAST_WEEK://上一周
+                callTimeObj.startTime = getLastWeekTimeStamp().start_time;
+                callTimeObj.endTime = getLastWeekTimeStamp().end_time;
+                break;
+            default:
+                callTimeObj.startTime = getTodayTimeStamp().start_time;
+                break;
         }
         return callTimeObj;
     }
+
+    handleDateChange = (value) => {
+        this.setState({currentDateType: value}, () => {
+            this.getCallTimeData();
+        });
+    };
 
     //获取呼出总时长的统计
     getCallTimeData() {
@@ -85,10 +149,18 @@ class TeamDataColumn extends React.Component {
             end_time: timeObj.endTime,
             device_type: 'all'
         };
+        this.setState({callTimeLoading: true, callTimeErrMsg: ''});
         myDataAjax.getCallTimeData(phoneParams).then((data) => {
-            this.setState({callTimeData: data});
+            this.setState({
+                callTimeData: data,
+                callTimeLoading: false
+            });
         }, (errorMsg) => {
-
+            this.setState({
+                callTimeLoading: false,
+                callTimeErrMsg: errorMsg,
+                callTimeData: []
+            });
         });
     }
 
@@ -160,56 +232,78 @@ class TeamDataColumn extends React.Component {
         let timeObj = this.getCallTimeObj();
         //只展示前三条数据
         callTimeData = _.filter(callTimeData, (item, index) => index <= 2);
+        const callTimeOptions = _.map(DATE_TYPE_MAP, date => (
+            <Option key={date.value} value={date.value}>{date.name}</Option>
+        ));
+        const cls = classNames('my-data-call-time-card', {
+            'has-loading': this.state.callTimeLoading
+        });
+
         const callTimeContent = (
             <div>
-                <div
-                    className='my-data-title'>{timeObj.callTimeDescr}{Intl.get('home.page.callout.time', '呼出总时长')}</div>
-                <div className='call-time-total my-data-title-data'>
-                    {time.timeDescr !== '0' ? (
-                        <span className="add-data-style phone-total-time phone-total-data">
-                            {time.hours > 0 ? <span className='num'>{time.hours}<span
-                                className="total-data-desc">{Intl.get('user.time.hour', '小时')} </span></span> : null}
-                            {time.minutes > 0 ?
-                                <span className='num'>{time.minutes}<span
-                                    className="total-data-desc">{Intl.get('user.time.minute', '分')} </span></span> : null}
-                            {time.second > 0 ? <span className='num'>{time.second}<span
-                                className="total-data-desc">{Intl.get('user.time.second', '秒')} </span></span> : null}
-                            {time.timeDescr === 0 ? time.timeDescr : null}
-                        </span>
-                    ) : (
-                        <span className="add-data-style phone-total-time phone-total-data">
-                            <span className='num'> 0 {Intl.get('user.time.second', '秒')}</span>
-                        </span>)}
+                <div className='my-data-title'>
+                    <div className="call-time-select-wrapper">
+                        <Select
+                            value={this.state.currentDateType}
+                            onChange={this.handleDateChange}
+                        >
+                            {callTimeOptions}
+                        </Select>
+                        <span>{Intl.get('home.page.callout.time', '呼出总时长')}</span>
+                    </div>
                 </div>
-                <div className='call-time-list my-data-detail-list'>
-                    {_.map(callTimeData, (item, index) => {
-                        //第一名是100%
-                        let percent = 100;
-                        //其他名次占第一名的百分比
-                        if (index !== 0) {
-                            percent = firstTotalTime ? (item.totalTime / firstTotalTime) * 100 : 0;
-                        }
-                        let timeObj = TimeUtil.secondsToHourMinuteSecond(item.totalTime || 0);
-                        return (
-                            <div className="call-time-item">
-                                <span className='call-time-name'
-                                    title={_.get(item, 'salesName', '')}>{_.get(item, 'salesName', '')}</span>
-                                <span className='progress-bar-wrap'>
-                                    <Tooltip title={timeObj.timeDescr} placement="left">
-                                        <Progress size='small' percent={percent} showInfo={false}
-                                            strokeColor={{
-                                                from: '#d3eafd',
-                                                to: '#a7d5fa'
-                                            }}
-                                            status='normal'/>
-                                    </Tooltip>
-                                </span>
-                            </div>);
-                    })}
-                </div>
+                {
+                    this.state.callTimeLoading ? <Spinner/> : (
+                        <div>
+                            <div className='call-time-total my-data-title-data'>
+                                {time.timeDescr !== '0' ? (
+                                    <span className="add-data-style phone-total-time phone-total-data">
+                                        {time.hours > 0 ? <span className='num'>{time.hours}<span
+                                            className="total-data-desc">{Intl.get('user.time.hour', '小时')} </span></span> : null}
+                                        {time.minutes > 0 ?
+                                            <span className='num'>{time.minutes}<span
+                                                className="total-data-desc">{Intl.get('user.time.minute', '分')} </span></span> : null}
+                                        {time.second > 0 ? <span className='num'>{time.second}<span
+                                            className="total-data-desc">{Intl.get('user.time.second', '秒')} </span></span> : null}
+                                        {time.timeDescr === 0 ? time.timeDescr : null}
+                                    </span>
+                                ) : (
+                                    <span className="add-data-style phone-total-time phone-total-data">
+                                        <span className='num'> 0 {Intl.get('user.time.second', '秒')}</span>
+                                    </span>)}
+                            </div>
+                            <div className='call-time-list my-data-detail-list'>
+                                {_.map(callTimeData, (item, index) => {
+                                    //第一名是100%
+                                    let percent = 100;
+                                    //其他名次占第一名的百分比
+                                    if (index !== 0) {
+                                        percent = firstTotalTime ? (item.totalTime / firstTotalTime) * 100 : 0;
+                                    }
+                                    let timeObj = TimeUtil.secondsToHourMinuteSecond(item.totalTime || 0);
+                                    return (
+                                        <div className="call-time-item">
+                                            <span className='call-time-name'
+                                                title={_.get(item, 'salesName', '')}>{_.get(item, 'salesName', '')}</span>
+                                            <span className='progress-bar-wrap'>
+                                                <Tooltip title={timeObj.timeDescr} placement="left">
+                                                    <Progress size='small' percent={percent} showInfo={false}
+                                                        strokeColor={{
+                                                            from: '#d3eafd',
+                                                            to: '#a7d5fa'
+                                                        }}
+                                                        status='normal'/>
+                                                </Tooltip>
+                                            </span>
+                                        </div>);
+                                })}
+                            </div>
+                        </div>
+                    )
+                }
             </div>);
         return (<DetailCard content={callTimeContent}
-            className='my-data-call-time-card'/>);
+            className={cls}/>);
     }
 
     renderContactCustomers() {
