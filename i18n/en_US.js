aa56f1df58819d02f1029f85e95b4ff1e4a7fa70
zhshj <1062735818@qq.com>
2019-09-10 09:42:46 +0800

    拜访申请加上待我审批

--- i18n/en_US.js
+++ i18n/en_US.js
@@ -3414,6 +3414,7 @@ var en_US = {
     'clue.recommend.input.keyword': 'Please input keyword',//请输入关键词
     'clue.recommend.extract.num.limit': 'The number of clues extracted by your organization today has reached the limit of {maxLimit}. Please come back tomorrow.',//您所在组织今天提取的线索数已达{maxLimit}条上限，请明天再来提取
     'clue.recommend.has.extract': 'Your organization has extracted {hasExtract} today and can extract up to {ableExtract} clues',//您所在的组织今天已经提取了{hasExtract}条，最多还能提取{ableExtract}条线索
-    'clue.customer.noty.all.list': 'Click to see all clues?'//查看所有线索？
+    'clue.customer.noty.all.list': 'Click to see all clues?',//查看所有线索？
+    'clue.customer.fail.get.all.lists': 'Failed to get all visit apply'//获取全部拜访申请失败
 };
 export default en_US;
