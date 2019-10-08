5e56f467260aa3e0b7abbb898e83dde0cf5d5a80
LemonyIce <879935228@qq.com>
2019-10-08 11:01:07 +0800

    点客户删除、释放操作时，提示的修改

--- portal/modules/crm/public/crm-list.js
+++ portal/modules/crm/public/crm-list.js
@@ -1939,7 +1939,7 @@ class Crm extends React.Component {
                                         </Button> : 
                                         <Popconfirm placement="topRight" 
                                             onConfirm={this.confirmDelete.bind(this,record.id)}
-                                            title={Intl.get('crm.customer.delete', '删除后，您可以从回收站中找回客户')}>
+                                            title={Intl.get('crm.customer.delete', '删除后，可以在回收站中找回该客户，您确定要删除吗？')}>
                                             <a className='release-customer'
                                                 title={Intl.get('common.delete', '删除')}>
                                                 <i className="iconfont icon-delete order-btn-class handle-btn-item"></i>
@@ -1949,7 +1949,7 @@ class Crm extends React.Component {
                             </span>
                             {userData.hasRole(userData.ROLE_CONSTANS.OPERATION_PERSON) ? null : (
                                 <Popconfirm placement="topRight" onConfirm={this.releaseCustomer.bind(this, record.id)}
-                                    title={Intl.get('crm.customer.release.confirm.tip', '释放到客户池后，其他人也可以查看、提取，您确认释放吗？')}>
+                                    title={Intl.get('crm.customer.release.confirm.tip', '释放到客户池后，其他人也可以查看、提取，您确定要释放吗？')}>
                                     <a className='release-customer'
                                         title={Intl.get('crm.customer.release', '释放')}>
                                         <i className="iconfont icon-release handle-btn-item"/>
