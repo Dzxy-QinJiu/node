5508c8b874248c7f64ea94663c204e20a303c84c
yubin <yubin@eefung.com>
2019-10-09 15:57:21 +0800

    线索模块自定义常用筛选展示时增加由条件到筛选项的转换

--- portal/modules/clue_customer/public/views/clue-filter-panel/index.js
+++ portal/modules/clue_customer/public/views/clue-filter-panel/index.js
@@ -95,12 +95,286 @@ class ClueFilterPanel extends React.Component {
             }
         })
             .done(result => {
-                const customCommonFilter = _.get(result, 'list');
+                const conditionList = _.get(result, 'list');
 
+                const customCommonFilter = _.map(conditionList, condition => this.getFilterItemFromConditionItem(condition));
 
                 this.setState({customCommonFilter});
             });
     }
+    
+    //将自定义常用筛选查询条件转换为前端展示用的格式
+    getFilterItemFromConditionItem(item) {
+        let filters = [];
+        let plainFilters = [];
+    
+        //将处理好的筛选项组装成FilterList所需的格式
+        const handleAddItem = nameObj => {
+            let filterItem = null;
+            plainFilters.push(nameObj);
+            filterItem = {
+                ...nameObj,
+                data: [{
+                    ...nameObj,
+                    selected: true
+                }]
+            };
+            const sameGroupItem = filters.find(x => x.groupId === filterItem.groupId);
+            //将已存在的高级筛选合并成commonData的结构
+            if (sameGroupItem) {
+                sameGroupItem.data.push({
+                    ...nameObj,
+                    selected: true
+                });
+            }
+            else {
+                filters.push(filterItem);
+            }
+        };
+    
+        //处理筛选项的value，处理成前端的格式
+        const handleValue = (value, key) => {
+            let item = null;
+            const nameObj = {
+                groupId: key,
+                groupName: key,
+                value: value,
+                name: value
+            };
+            //处理value（实际的筛选项值）
+            nameObj.value = value;
+            if (_.get(value, 'sale_stages')) {
+                nameObj.value = value.sale_stages;
+                nameObj.name = value.sale_stages;
+            }
+            //处理name（展示的筛选项文字）
+            switch (key) {
+                case 'sales_team_id':
+                    item = this.state.teamList.find(x => x.group_id === value);
+                    if (item) {
+                        nameObj.name = item.group_name;
+                    }
+                    break;
+                case 'contain_sales_opportunity'://订单阶段value由name表示
+                    item = stageArray.find(x => x.name === value);
+                    if (item) {
+                        nameObj.name = item.show_name;
+                    }
+                    if (value === 'false') {
+                        nameObj.name = UNKNOWN;
+                        nameObj.value = UNKNOWN;
+                    }
+                    nameObj.groupId = 'sales_opportunities';
+                    break;
+                case 'sales_opportunities':
+                    item = stageArray.find(x => x.name === value);
+                    if (item) {
+                        nameObj.name = item.show_name;
+                    }
+                    break;
+                case 'administrative_level':
+                    item = filterLevelArray.find(x => x.id === value);
+                    if (item) {
+                        nameObj.name = item.level;
+                    }
+                    break;
+                case 'immutable_labels' :
+                    item = this.systemTagList.find(x => x.name === value);
+                    if (item) {
+                        nameObj.name = item.show_name;
+                    }
+                    break;
+                case 'labels':
+                    item = this.tagList.find(x => x.name === value);
+                    if (item) {
+                        nameObj.name = item.show_name;
+                    }
+                    if (nameObj.name === Intl.get('crm.tag.unknown', '未打标签的客户')) {
+                        nameObj.selectOnly = true;
+                    }
+                    break;
+                case 'competing_products':
+                    item = this.competitorList.find(x => x.name === value);
+                    if (item) {
+                        nameObj.name = item.show_name;
+                    }
+                    break;
+                case 'qualify_label':
+                    item = qualifiedTagList.find(x => x.value === value);
+                    if (item) {
+                        nameObj.name = item.name;
+                    }
+                    break;
+                case 'contain_contact':
+                    nameObj.name = Intl.get('crm.no.contact.way', '无联系方式客户');
+                    nameObj.value = 'no_contact_way';
+                    nameObj.groupId = COMMON_OTHER_ITEM;
+                    nameObj.groupName = Intl.get('crm.186', '其他');
+                    break;
+                case 'call_and_remark':
+                    nameObj.name = Intl.get('crm.call.no.remark', '最后联系但未写跟进记录');
+                    nameObj.value = 'last_call_no_record';
+                    nameObj.groupId = COMMON_OTHER_ITEM;
+                    nameObj.groupName = Intl.get('crm.186', '其他');
+                    break;
+                case 'last_trace':
+                    nameObj.name = Intl.get('crm.call.no.remark.over30', '超30天未写跟进记录');
+                    nameObj.value = 'last_trace';
+                    nameObj.groupId = COMMON_OTHER_ITEM;
+                    nameObj.groupName = Intl.get('crm.186', '其他');
+                    break;
+                case 'interest_member_ids':
+                    nameObj.name = Intl.get('crm.my.concerned.customer', '我关注的客户');
+                    nameObj.value = 'my_interest';
+                    nameObj.groupId = COMMON_OTHER_ITEM;
+                    nameObj.groupName = Intl.get('crm.186', '其他');
+                    break;
+                case 'unexist_fields':
+                    switch (value) {
+                        case 'industry':
+                            nameObj.name = UNKNOWN;
+                            nameObj.value = UNKNOWN;
+                            nameObj.groupId = 'industry';
+                            break;
+                        case 'province':
+                            nameObj.groupId = 'province';
+                            nameObj.name = UNKNOWN;
+                            nameObj.value = UNKNOWN;
+                            break;
+                        case 'qualify_label':
+                            nameObj.groupId = 'qualify_label';
+                            nameObj.name = CUSTOMER_TAGS.NEVER_QUALIFIED;
+                            nameObj.value = '3';
+                            break;
+                        case 'labels':
+                            nameObj.groupId = 'labels';
+                            nameObj.name = Intl.get('crm.tag.unknown', '未打标签的客户');
+                            nameObj.value = SPECIAL_LABEL.NON_TAGGED_CUSTOMER;
+                            break;
+                        case 'member_id':
+                            nameObj.groupId = 'member_id';
+                            nameObj.name = Intl.get('crm.213', '未分配客户');
+                            nameObj.value = OTHER_FILTER_ITEMS.UNDISTRIBUTED;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case 'exist_fields':
+                    switch (value) {
+                        case 'interest_member_ids':
+                            nameObj.name = Intl.get('crm.concerned.customer', '被关注的客户');
+                            nameObj.value = 'interest_member_ids';
+                            nameObj.groupId = COMMON_OTHER_ITEM;
+                            nameObj.groupName = Intl.get('crm.186', '其他');
+                            break;
+    
+                        default:
+                            break;
+                    }
+                    break;
+                case 'availability':
+                    nameObj.groupId = COMMON_OTHER_ITEM;
+                    nameObj.name = Intl.get('crm.available.customer', '有效客户');
+                    nameObj.value = 'availability';
+                    break;
+            }
+            handleAddItem(nameObj);
+        };
+    
+        if (_.get(item, 'query_condition')) {
+            if (_.get(item.query_condition, 'query')) {
+                _.each(item.query_condition.query, (value, key) => {
+                    if (value) {
+                        let valueList = [];
+                        if (value.length) {
+                            valueList = value;
+                        }
+                        if (typeof value === 'string') {
+                            //拼接字符串（数组value）
+                            if (value.includes(',')) {
+                                valueList = value.split(',');
+                            }
+                            //单个字符串
+                            else {
+                                handleValue(value, key);
+                            }
+                        }
+                        //数组value
+                        if (Array.isArray(valueList) && valueList.length > 0) {
+                            valueList.forEach(x => {
+                                handleValue(x, key);
+                            });
+                        }
+                    }
+                });
+            }
+            //日期范围通过interval判断
+            if (_.get(item.query_condition, 'rang_params.length')) {
+                item.query_condition.rang_params.forEach(rangeItem => {
+                    const nameObj = {
+                        groupId: COMMON_OTHER_ITEM,
+                        groupName: Intl.get('crm.186', '其他'),
+                        value: '',
+                        name: rangeItem.name
+                    };
+                    switch (rangeItem.name) {
+                        case 'last_contact_time':
+                            switch (rangeItem.interval) {
+                                case 30:
+                                    nameObj.name = Intl.get('crm.over.day.without.contact', '超{day}天未联系', { day: 30 });
+                                    nameObj.value = OTHER_FILTER_ITEMS.THIRTY_UNCONTACT;
+                                    //超30天未联系的客户
+                                    break;
+                                case 15:
+                                    nameObj.name = Intl.get('crm.over.day.without.contact', '超{day}天未联系', { day: 15 });
+                                    nameObj.value = OTHER_FILTER_ITEMS.FIFTEEN_UNCONTACT;
+                                    //超15天未联系的客户
+                                    break;
+                                case 7:
+                                    nameObj.name = Intl.get('crm.over.day.without.contact', '超{day}天未联系', { day: 7 });
+                                    nameObj.value = OTHER_FILTER_ITEMS.SEVEN_UNCONTACT;
+                                    //超7天未联系的客户
+                                    break;
+                            }
+                            break;
+                        case 'last_login_time':
+                            switch (rangeItem.interval) {
+                                case 30:
+                                    nameObj.name = Intl.get('crm.recent.month.active', '近一个月的活跃客户');
+                                    nameObj.value = 'month_login';
+                                    //超30天未联系的客户
+                                    break;
+                                case 7:
+                                    nameObj.name = Intl.get('crm.recent.week.active', '近一周的活跃客户');
+                                    nameObj.value = 'seven_login';
+                                    //超7天未联系的客户
+                                    break;
+                            }
+                            break;
+                        case 'sales_opportunity_count':
+                            nameObj.name = Intl.get('crm.order.more.customer', '多个订单的客户');
+                            nameObj.value = OTHER_FILTER_ITEMS.MULTI_ORDER;
+                            break;
+                        //开始时间跳过处理
+                        case 'start_time':
+                            return false;
+                        default:
+                            break;
+                    }
+                    handleAddItem(nameObj);
+                });
+            }
+        }
+    
+        return {
+            name: item.name,
+            value: item.name,
+            data: filters,
+            plainFilterList: plainFilters,
+            id: item.id
+        };
+    };
 
     //删除自定义常用筛选
     deleteCustomCommonFilter(item) {
@@ -280,13 +554,7 @@ class ClueFilterPanel extends React.Component {
         });
 
         //将自定义常用筛选加到常用筛选数据中
-        _.each(this.state.customCommonFilter, item => {
-            commonData.push({
-                name: item.name,
-                value: item.name,
-                id: item.id
-            });
-        });
+        commonData = commonData.concat(this.state.customCommonFilter)
 
         const advancedData = [{
             groupName: Intl.get('crm.96', '地域'),
