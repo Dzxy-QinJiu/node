6cfbe97370e773298f32660ebaf0c54bcf5a8931
huzhenli <huzhenli@eefung.com>
2019-10-09 14:29:18 +0800

    变更记录，代码优化

--- portal/modules/app_user_manage/public/action/user-detail-change-record-actions.js
+++ portal/modules/app_user_manage/public/action/user-detail-change-record-actions.js
@@ -9,8 +9,6 @@ function UserDetailChangeRecordAction() {
     this.generateActions(
         // 获取用户详情变更记录
         'setApp',
-        // 获取用户的app列表
-        'getUserApp',
     );
     this.getUserDetailChangeRecord = function(searchObj) {
         this.dispatch({loading: true, error: false});
@@ -18,27 +16,6 @@ function UserDetailChangeRecordAction() {
             this.dispatch({loading: false, error: false, data: data});
         }, (errorMsg) => {
             this.dispatch({loading: false, error: true, errorMsg: errorMsg});
-        });
-    };
-    // 根据用户的ID获取应用列表appId和应用的名称
-    this.getUserApp = function(userId,callback) {
-        var dataObj = {
-            app_name: '',//下拉框中显示app的名字
-            app_id: '',
-            data: [],//当前用户所拥有的app列表
-        };
-        this.dispatch({loading: true,error: false});
-        userDetailChangeRecordAjax.getSingleUserAppList(userId).then((result) => {
-
-            if (_.isArray(result.apps) && result.apps.length >= 1) {
-                dataObj.app_name = result.apps[0].app_name;
-                dataObj.app_id = result.apps[0].app_id;
-                dataObj.data = result.apps;
-                callback && callback(dataObj);
-            }
-            this.dispatch({loading: false,error: false, dataObj: dataObj});
-        }, (errorMsg) => {
-            this.dispatch({loading: false,error: true, errorMsg: errorMsg});
         });
     };
 }
