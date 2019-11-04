f0279188e18c1b526a6bbec42f7af422e1e1ac56
sunqingfeng <sunqingfeng@curtao.com>
2019-11-04 10:27:09 +0800

    根据是否开通营收中心判断合同分析展示

--- portal/modules/analysis/public/pages/contract/index.js
+++ portal/modules/analysis/public/pages/contract/index.js
@@ -4,6 +4,7 @@
 
 import { getContextContent } from '../../utils';
 import {CONTRACT_MENUS} from '../../consts';
+import {isOpenCash} from 'PUB_DIR/sources/utils/common-method-util';
 
 //引入同目录下以.js结尾的并且不包含index的文件
 const req = require.context('.', false, /^((?!index).)*\.js$/);
@@ -15,9 +16,10 @@ module.exports = {
     title: CONTRACT_MENUS.INDEX.name,
     key: CONTRACT_MENUS.INDEX.key,
     menuIndex: 1,
-    privileges: [
-        'OPLATE_CONTRACT_ANALYSIS',
-    ],
+    isShowCallback: () => {
+        //开通营收中心
+        return isOpenCash();
+    },
     pages,
 };
 
