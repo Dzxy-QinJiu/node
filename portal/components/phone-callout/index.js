4a0f45e4b6679a495d711e6eb459741b51b917b9
tmq520 <1005275284@qq.com>
2019-11-12 18:32:21 +0800

    拨打电话按钮里个人正式版的呼叫禁用判断

--- portal/components/phone-callout/index.js
+++ portal/components/phone-callout/index.js
@@ -38,13 +38,9 @@ class PhoneCallout extends React.Component {
         });
     };
     handleVisibleChange = (phoneNumber, contactName,visible) => {
-        if (visible && hasCalloutPrivilege()){
-            //如果是个人正式版，需要提示升级为企业版才能拨打号码
-            let versionAndType = checkVersionAndType();
-            if(versionAndType.isPersonalFormal) {
-                this.setState({visible});
-                return;
-            }
+        //如果是个人正式版，需要提示升级为企业版才能拨打号码
+        let versionAndType = checkVersionAndType();
+        if (visible && hasCalloutPrivilege() && !versionAndType.isPersonalFormal){// 显示，并且能拨打电话，以及不是个人正式版时
             if (!this.state.ableClickPhoneIcon){
                 return;
             }
@@ -69,7 +65,7 @@ class PhoneCallout extends React.Component {
         var contentTip = showDisabledCallTip();
         //如果是个人正式版，需要提示升级为企业版才能拨打号码
         let versionAndType = checkVersionAndType();
-        if(versionAndType.personal && versionAndType.formal && hasCalloutPrivilege()) {
+        if(versionAndType.personal && versionAndType.formal) {
             contentTip = Intl.get('payment.please.upgrade.company.version', '请先升级为企业版。您可以联系我们的销售：{contact}',{contact: '400-6978-520'});
         }
         var titleTip = Intl.get('crm.click.call.phone', '点击拨打电话');
