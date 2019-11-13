cae0cecd308ba70ede1b8cfd7fd079c93ab44710
yubin <yubin@eefung.com>
2019-11-13 09:20:58 +0800

    版权信息截至时间改为2019

--- portal/components/clue-to-customer-panel/consts.js
+++ portal/components/clue-to-customer-panel/consts.js
@@ -1,6 +1,6 @@
 /**
- * Copyright (c) 2015-2018 EEFUNG Software Co.Ltd. All rights reserved.
- * 版权所有 (c) 2015-2018 湖南蚁坊软件股份有限公司。保留所有权利。
+ * Copyright (c) 2015-2019 EEFUNG Software Co.Ltd. All rights reserved.
+ * 版权所有 (c) 2015-2019 湖南蚁坊软件股份有限公司。保留所有权利。
  * Created by yubin on 2019/11/11.
 */
 
