d91428a32525cdbd66d7d4c765a524f4abfa7fd7
zhangshujuan <zhangshujuan@eefung.com>
2019-11-19 09:16:09 +0800

    代码走查问题的修改

--- portal/modules/clue_customer/public/store/clue-filter-store.js
+++ portal/modules/clue_customer/public/store/clue-filter-store.js
@@ -67,7 +67,7 @@ ClueFilterStore.prototype.setInitialData = function() {
     this.teamMemberList = [];
     //筛选相似线索
     this.filterLabels = '';
-    //todo 如果是销售领导或者销售角色或者运营 默认选中 待我处理 进行筛选  这个功能暂时隐藏
+    //如果是销售领导或者销售角色或者运营 默认选中 待我处理 进行筛选  这个功能暂时隐藏
     //this.filterAllotNoTraced = getClueUnhandledPrivilege() ? '0' : '';
     this.filterAllotNoTraced = '';
     //未打通电话的线索
