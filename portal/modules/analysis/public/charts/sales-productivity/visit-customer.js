17cdfc1f1265beb9db1f262b16d77488cb586e39
yubin <yubin@eefung.com>
2019-11-25 17:38:46 +0800

    点击销售展示客户

--- portal/modules/analysis/public/charts/sales-productivity/visit-customer.js
+++ portal/modules/analysis/public/charts/sales-productivity/visit-customer.js
@@ -2,7 +2,6 @@
  * 拜访客户统计
  */
 
-import { argCallbackMemberIdToMemberIds } from '../../utils';
 let conditionCache = {};
 
 export function getVisitCustomerChart() {
@@ -11,63 +10,60 @@ export function getVisitCustomerChart() {
         chartType: 'table',
         url: '/rest/analysis/callrecord/v1/customertrace/:data_type/sale/trace/statistics',
         argCallback: arg => {
-            argCallbackMemberIdToMemberIds(arg);
             arg.query.result_type = 'user';
 
             conditionCache = arg.query;
         },
-        processData: data => {
+        processData: (data, chart, analysisInstance, chartIndex) => {
+            chart.option = {
+                columns: [{
+                    title: '日期',
+                    dataIndex: 'sales_team',
+                    width: '10%',
+                }, {
+                    title: '出差人员',
+                    dataIndex: 'nick_name',
+                    width: '10%',
+                    render: (value, record) => {
+                        return <span onClick={onSalesNameClick.bind(this, record, analysisInstance, chartIndex)}>{value}</span>;
+                    }
+                }]
+            };
+
             const list = _.get(data, 'list');
-            return _.map(list, item => {
-                ['visit', 'phone_all', 'phone_answer'].forEach(field => {
-                    if (isNaN(item[field])) item[field] = 0;
-                });
-
-                item.phone_no_answer = item.phone_all - item.phone_answer;
-
-                return item;
-            });
-        },
-        option: {
-            columns: [{
-                title: '日期',
-                dataIndex: 'sales_team',
-                width: '10%',
-            }, {
-                title: '出差人员',
-                dataIndex: 'nick_name',
-                width: '10%',
-                render: value => {
-                    return <span onClick={onSalesNameClick}>{value}</span>;
-                }
-            }]
+            return _.filter(list, item => item.visit > 0);
         },
     };
 
     //销售人员名点击事件
-    function onSalesNameClick(id, e) {
-        console.log(1);
-        conditionCache.member_id = id;
+    function onSalesNameClick(record, analysisInstance, chartIndex) {
+        let charts = analysisInstance.state.charts;
 
-        const conditions = _.map(conditionCache, (value, key) => ({name: key, value}));
+        let chart = charts[chartIndex];
 
-        const paramObj = {
-            listType: 'customer',
-            url: '/rest/analysis/callrecord/v1/customertrace/sale/visit/statistics',
-            conditions,
-            columns: [
-                {
-                    title: '日期',
-                    dataIndex: 'customer_name',
-                    width: '10%'
-                },
-                {
-                    title: '拜访客户',
-                    dataIndex: 'customer_name',
-                    width: '10%'
-                },
-            ],
-        };
+        chart.title = <div>拜访客户频率统计<span>返回</span></div>;
+        chart.url = '/rest/analysis/callrecord/v1/customertrace/sale/visit/statistics';
+
+        conditionCache.member_id = record.user_id;
+
+        chart.conditions = _.map(conditionCache, (value, key) => ({name: key, value}));
+
+        chart.processData = data => data.list;
+
+        chart.option.columns = [
+            {
+                title: '日期',
+                dataIndex: 'customer_name',
+                width: '10%'
+            },
+            {
+                title: '拜访客户',
+                dataIndex: 'customer_name',
+                width: '10%'
+            },
+        ];
+
+        analysisInstance.getData(chartIndex);
     }
 
     return chart;
