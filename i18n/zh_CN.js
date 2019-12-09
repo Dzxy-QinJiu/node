fe63e7374b7ff9886a34aea65374b60aea689eb5
tmq520 <1005275284@qq.com>
2019-12-09 18:50:02 +0800

    验证错误提示修改

--- i18n/zh_CN.js
+++ i18n/zh_CN.js
@@ -299,6 +299,7 @@ var zh_CN = {
     'common.no.more.data.tips': '没有更多{name}了',
     'common.no.data.tips': '暂无{name}',
     'common.call.owe.tips': '您的电话号码已欠费，请充值后再试！',
+    'common.number.validate.tips': '请填写最多两位小数的数字',
 
     //公共部分--end
     'login.login': '登录',
