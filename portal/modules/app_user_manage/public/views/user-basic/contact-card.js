e20b88a9b3a13cf5af2b816554133d1866fcf97c
huzhenli <huzhenli@eefung.com>
2019-12-14 16:23:01 +0800

    用户详情中，联系方式以及所属组织，没有内容的处理

--- portal/modules/app_user_manage/public/views/user-basic/contact-card.js
+++ portal/modules/app_user_manage/public/views/user-basic/contact-card.js
@@ -7,7 +7,7 @@ import DetailCard from 'CMP_DIR/detail-card';
 import { PropTypes } from 'prop-types';
 import { hasPrivilege } from 'CMP_DIR/privilege/checker';
 var UserDetailEditField = require('CMP_DIR/basic-edit-field-new/input');
-
+import userManagePrivilege from '../../privilege-const';
 
 class ContactCard extends React.Component {
     constructor(props) {
@@ -34,38 +34,47 @@ class ContactCard extends React.Component {
     }
     render() {
         const { userInfo } = this.props;
+        const hasEditAuth = hasPrivilege(userManagePrivilege.USER_MANAGE);
         return (
             <DetailCard
                 className='contact-card-container'
                 title={Intl.get('crm.5', '联系方式')}
                 content={(
                     <div className="sales-team-show-block">
-                        <div className="sales-team clearfix">
-                            <span className="sales-team-label">
-                                {Intl.get('common.email', '邮箱')}:
-                            </span>
-                            <span className="sales-team-text">
-                                <UserDetailEditField
-                                    id={userInfo.user_id}
-                                    saveEditInput={this.props.saveEditInput}
-                                    showBtn={true}
-                                    {...this.props.email}
-                                />
-                            </span>
-                        </div>
-                        <div className="sales-team clearfix">
-                            <span className="sales-team-label">
-                                {Intl.get('user.phone', '手机号')}:
-                            </span>
-                            <span className="sales-team-text">
-                                <UserDetailEditField
-                                    id={userInfo.user_id}
-                                    saveEditInput={this.props.saveEditInput}
-                                    showBtn={true}
-                                    {...this.props.phone}
-                                />
-                            </span>
-                        </div>
+                        {
+                            hasEditAuth || this.props.email.value ? (
+                                <div className="sales-team clearfix">
+                                    <span className="sales-team-label">
+                                        {Intl.get('common.email', '邮箱')}:
+                                    </span>
+                                    <span className="sales-team-text">
+                                        <UserDetailEditField
+                                            id={userInfo.user_id}
+                                            saveEditInput={this.props.saveEditInput}
+                                            showBtn={true}
+                                            {...this.props.email}
+                                        />
+                                    </span>
+                                </div>
+                            ) : null
+                        }
+                        {
+                            hasEditAuth || this.props.phone.value ? (
+                                <div className="sales-team clearfix">
+                                    <span className="sales-team-label">
+                                        {Intl.get('user.phone', '手机号')}:
+                                    </span>
+                                    <span className="sales-team-text">
+                                        <UserDetailEditField
+                                            id={userInfo.user_id}
+                                            saveEditInput={this.props.saveEditInput}
+                                            showBtn={true}
+                                            {...this.props.phone}
+                                        />
+                                    </span>
+                                </div>
+                            ) : null
+                        }
                     </div>
                 )}
             />
