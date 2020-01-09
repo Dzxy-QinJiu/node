6ac6aaca3d174b46235e4ce303e8fb92c1c946e1
zhangshujuan <zhangshujuan@eefung.com>
2020-01-09 16:48:00 +0800

    走查问题补充

--- portal/public/sources/push/index.js
+++ portal/public/sources/push/index.js
@@ -15,7 +15,7 @@ var phoneMsgEmitter = require('../../../public/sources/utils/emitters').phoneMsg
 var phoneEmitter = require('../../../public/sources/utils/emitters').phoneEmitter;
 let ajaxGlobal = require('../jquery.ajax.global');
 var hasPrivilege = require('../../../components/privilege/checker').hasPrivilege;
-import {SYSTEM_NOTICE_TYPE_MAP, KETAO_SYSTEM_NOTICE_TYPE_MAP, SYSTEM_NOTICE_TYPES,APPLY_APPROVE_TYPES, DIFF_APPLY_TYPE_UNREAD_REPLY,CALL_TYPES,CLUEEXTRACTTYPE} from '../utils/consts';
+import {SYSTEM_NOTICE_TYPE_MAP, KETAO_SYSTEM_NOTICE_TYPE_MAP, SYSTEM_NOTICE_TYPES,APPLY_APPROVE_TYPES, DIFF_APPLY_TYPE_UNREAD_REPLY,CALL_TYPES,CLUE_EXTRACT_TYPE} from '../utils/consts';
 import logoSrc from './notification.png';
 import userData from '../user-data';
 import Trace from 'LIB_DIR/trace';
@@ -178,7 +178,11 @@ window.openAllClues = function(){
 };
 //是自己提取的线索
 function isExtractByMe(data) {
-    return _.includes(CLUEEXTRACTTYPE, _.get(data,'type')) && _.get(data,'operator_id','') === userData.getUserData().user_id;
+    //如果是推荐线索提取的类型或者是线索池提取的类型
+    const extractType = _.includes(CLUE_EXTRACT_TYPE, _.get(data,'type'));
+    //并且是登录人操作的
+    const isOperatedByMe = _.get(data,'operator_id','') === userData.getUserData().user_id;
+    return extractType && isOperatedByMe;
 }
 //处理线索的数据
 function clueUnhandledListener(data) {
