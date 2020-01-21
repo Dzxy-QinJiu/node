a60f483c57809525bae08dc7c4af525c9060f857
xueyanqing <xueyanqing@curtao.com>
2020-01-21 09:35:51 +0800

    添加注释

--- portal/modules/clue_customer/public/index.js
+++ portal/modules/clue_customer/public/index.js
@@ -909,7 +909,7 @@ class ClueCustomer extends React.Component {
                     typeFilter.customer_similarity = Intl.get( 'clue.has.similar.customer','有相似客户');
                 }
             }
-            if(filterStoreData.appliedTryLead){
+            if(filterStoreData.appliedTryLead){ //筛选申请试用企业版的线索
                 typeFilter.version_upgrade_label = 'true';
             }
 
