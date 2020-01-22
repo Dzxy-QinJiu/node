dd78953a2d3a62a2ac28b698aedad009ea7a0cc4
tmq520 <1005275284@qq.com>
2020-01-22 09:38:22 +0800

    获取无效电话接口的权限修改，以及将无效电话存入localStorage.session中

--- portal/components/audio-report-function/index.js
+++ portal/components/audio-report-function/index.js
@@ -7,7 +7,9 @@
 import AudioPlayer from 'CMP_DIR/audioPlayer';
 import { addInvalidPhone, getInvalidPhone } from 'LIB_DIR/utils/invalidPhone';
 import commonMethodUtil from 'PUB_DIR/sources/utils/common-method-util';
-
+import {storageUtil} from 'ant-utils';
+const session = storageUtil.session;
+const INVALID_PHONE_KEY = 'invalid_phone_lists';
 
 class AudioReportFunction extends React.Component{
     constructor(props) {
@@ -18,7 +20,6 @@ class AudioReportFunction extends React.Component{
             curPlayItem: props.curPlayItem,//当前播放的录音所属的通话记录信息
             isShowReportButton: false,//是否显示上报按钮
             playingItemAddr: playItemAddr,//正在播放的录音的地址
-            invalidPhoneLists: [],//无效电话列表
             getInvalidPhoneErrMsg: '',//获取无效电话失败后的信息
             playingItemPhone: '',//正在听的录音所属的电话号码
             isAddingInvalidPhone: false,//正在添加无效电话
@@ -45,23 +46,30 @@ class AudioReportFunction extends React.Component{
         return commonMethodUtil.getAudioRecordUrl(item.local, item.recording, item.type);
     };
 
+    getInvalidPhoneLists = () => {
+        let invalidPhoneLists = session.get(INVALID_PHONE_KEY);
+        return invalidPhoneLists ? JSON.parse(invalidPhoneLists) : [];
+    };
+
     getInvalidPhone = () => {
         let item = this.state.curPlayItem;
         let playItemAddr = this.getPlayItemAddr(item);
+        let invalidPhoneLists = this.getInvalidPhoneLists();
         //一开始隐藏掉上报按钮，通过获取电话是否已上报，判断显示按钮
-        if(!_.includes(this.state.invalidPhoneLists, item.dst)) {
+        if(!_.includes(invalidPhoneLists, item.dst)) {
             getInvalidPhone({number: item.dst}, (data) => {
                 if(!_.get(data, 'total')) {//没有上报过时，显示上报按钮
                     this.setState({
                         isShowReportButton: true,
                     });
                 }else {
-                    let invalidPhoneLists = this.state.invalidPhoneLists;
                     invalidPhoneLists.push(item.dst);
-                    this.setState({
-                        invalidPhoneLists
-                    });
+                    session.set(INVALID_PHONE_KEY, JSON.stringify(invalidPhoneLists));
                 }
+            }, () => {
+                this.setState({
+                    isShowReportButton: true
+                });
             });
         }
         this.setState({
@@ -80,11 +88,12 @@ class AudioReportFunction extends React.Component{
             isAddingInvalidPhone: true
         });
         addInvalidPhone({'number': curPhone},() => {
-            this.state.invalidPhoneLists.push(curPhone);
+            let invalidPhoneLists = this.getInvalidPhoneLists();
+            invalidPhoneLists.push(curPhone);
+            session.set(INVALID_PHONE_KEY, JSON.stringify(_.uniq(invalidPhoneLists)));
             this.setState({
                 isShowReportButton: false,
                 isAddingInvalidPhone: false,
-                invalidPhoneLists: this.state.invalidPhoneLists,
                 addingInvalidPhoneErrMsg: ''
             });
         },(errMsg) => {
