3a7ec7b1d1d0b389665f5f53eaa511ea521ed793
yubin <yubin@eefung.com>
2020-02-13 11:56:33 +0800

    请假统计在月报里能显示了

--- portal/modules/analysis/public/charts/workflow/offduty.js
+++ portal/modules/analysis/public/charts/workflow/offduty.js
@@ -9,46 +9,25 @@ export function getOffdutyChart(paramObj) {
         title,
         chartType: 'table',
         url: '/rest/base/v1/workflow/offduty/statistics',
-        argCallback: arg => {
-            if (arg.query.member_ids) {
-                arg.query.user_id = arg.query.member_ids;
-
-                delete arg.query.member_ids;
-            }
-        },
-        processData: data => {
-            let processedData = [];
-
-            //出差次数，返回值应为整数
-            const businessTripNum = _.get(data, [0]);
-            //请假时长，返回值应为毫秒
-            const askForLeaveDuration = _.get(data, [1]);
-
-            if (_.isNumber(businessTripNum)) {
-                processedData.push({
-                    value: '出差次数: ' + businessTripNum
-                });
-            }
-
-            if (_.isNumber(askForLeaveDuration)) {
-                processedData.push({
-                    value: '请假天数: ' + askForLeaveDuration
-                });
-            }
-
-            return processedData;
-        },
-        processCsvData: (chart, option) => {
-            return _.map(option.dataSource, item => {
-                return [item.value];
-            });
-        },
+        conditions: [{
+            name: 'type',
+            value: type
+        }],
+        dataField: 'list',
         option: {
-            columns: [
-                {
-                    dataIndex: 'value'
-                }
-            ],
+            columns: getColumns(type)
         },
     };
+
+    function getColumns(type) {
+        let columns = [{
+            title: '团队',
+            dataIndex: 'team_name'
+        }, {
+            title: '销售',
+            dataIndex: 'nickname'
+        }];
+
+        return columns;
+    }
 }
