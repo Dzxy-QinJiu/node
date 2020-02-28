95c03c248e451e1fadc8bd4ccbce0f9f700ff1f0
tmq520 <1005275284@qq.com>
2020-02-28 20:03:01 +0800

    加上注释

--- portal/modules/clue_customer/public/action/clue-customer-action.js
+++ portal/modules/clue_customer/public/action/clue-customer-action.js
@@ -48,7 +48,7 @@ function ClueCustomerActions() {
         'remarkLeadExtractedByOther',//给被别人提取过的线索加一个标识
         'initialRecommendClues',//初始化推荐线索相关条件及状态
         'afterNewExtract',//提取推荐线索后
-        'setHotSource',
+        'setHotSource',//设置热门选项
     );
     //获取销售列表
     this.getSalesManList = function(cb) {
