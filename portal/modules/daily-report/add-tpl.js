c2ce93e395730a8c07978c6e7b822a6bf4ff87b4
yubin <yubin@eefung.com>
2020-02-28 15:04:20 +0800

    添加汇报模板视图改为用高价组件包裹

--- portal/modules/daily-report/add-tpl.js
+++ portal/modules/daily-report/add-tpl.js
@@ -4,6 +4,7 @@
 
 import { Button, Steps, Radio } from 'antd';
 import { VIEW_TYPE } from './consts';
+import addTplHoc from './add-tpl-hoc';
 
 const { Step } = Steps;
 
@@ -15,30 +16,19 @@ class AddTpl extends React.Component {
     render() {
         return (
             <div>
-                两步即可实现汇总和查看销售日常工作情况
-                <Steps current={this.props.currentStep}>
-                    <Step title="1. 选择模板" />
-                    <Step title="2. 设置规则" />
-                </Steps>
                 <Radio.Group onChange={this.onChange} value={this.state.value}>
                     <Radio value={1}>
                         <a href="javascript:void:0" onClick={() => { this.props.updateState({ currentView: VIEW_TYPE.EDIT_TPL }); }}>销售经理日报</a>
                     </Radio>
                 </Radio.Group>
-                <br/>
                 <div
                     onClick={() => { this.props.updateState({ currentView: VIEW_TYPE.ADD_NEW_TPL }); }}
                 >
                     添加新模板
                 </div>
-                <Button
-                    onClick={() => { this.props.updateState({ currentView: VIEW_TYPE.SET_RULE, currentStep: 2 }); }}
-                >
-                    下一步
-                </Button>
             </div>
         );
     }
 }
 
-export default AddTpl;
+export default addTplHoc(AddTpl);
