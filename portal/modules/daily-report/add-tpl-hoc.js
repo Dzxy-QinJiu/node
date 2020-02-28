b9b2319aed60b3c9cfcf1c7e4ce0365ae3978446
yubin <yubin@eefung.com>
2020-02-28 15:03:09 +0800

    增加添加模板高价组件

---
+++ portal/modules/daily-report/add-tpl-hoc.js
@@ -0,0 +1,42 @@
+/**
+ * 添加汇报模板高价组件
+ */
+
+import { Steps, Button } from 'antd';
+
+const { Step } = Steps;
+
+export default function(WrappedComponent) {
+    return class extends React.Component {
+        render() {
+            return (
+                <div>
+                    <Steps current={this.props.currentStep}>
+                        <Step title="1. 选择模板" />
+                        <Step title="2. 设置规则" />
+                    </Steps>
+                    <div>
+                        <WrappedComponent {...this.props} />
+                    </div>
+                    <div>
+                        <Button
+                            onClick={() => { this.props.updateState({ currentView: VIEW_TYPE.ADD_TPL, currentStep: 1 }); }}
+                        >
+                            上一步
+                        </Button>
+                        <Button
+                            onClick={() => { hideReportPanel(); }}
+                        >
+                            保存
+                        </Button>
+                        <Button
+                            onClick={() => { this.props.updateState({ currentView: VIEW_TYPE.SET_RULE, currentStep: 2 }); }}
+                        >
+                            下一步
+                        </Button>
+                    </div>
+                </div>
+            );
+        }
+    };
+}
