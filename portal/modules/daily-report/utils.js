076c8da70fa35466a1d4f405c92cde329dcff024
yubin <yubin@eefung.com>
2020-03-03 16:55:14 +0800

    增加保存模板的函数

--- portal/modules/daily-report/utils.js
+++ portal/modules/daily-report/utils.js
@@ -3,29 +3,14 @@ import { message } from 'antd';
 import { detailPanelEmitter } from 'PUB_DIR/sources/utils/emitters';
 const { getLocalWebsiteConfig, setWebsiteConfig } = require('LIB_DIR/utils/websiteConfig');
 const SITE_CONGFIG_KEY = 'is_no_longer_show_check_report_notice';
-
-//获取模板列表
-export function getTplList(callback) {
-    if (!_.isFunction(callback)) return;
-
-    ajax.send({
-        url: '/rest/customer/v3/dailyreport/templates'
-    })
-        .done(result => {
-            callback(result);
-        })
-        .fail(err => {
-            message.error(err);
-            callback();
-        });
-}
+const TPL_URL = '/rest/customer/v3/dailyreport/templates';
 
 //获取报告列表
 export function getReportList(callback) {
     if (!_.isFunction(callback)) return;
 
     ajax.send({
-        url: '/rest/customer/v3/dailyreport/templates'
+        url: TPL_URL
     })
         .done(result => {
             result = _.get(localStorage, 'daily-report.report-list');
@@ -55,4 +40,37 @@ export function showReportPanel() {
 //隐藏报告面板
 export function hideReportPanel() {
     detailPanelEmitter.emit(detailPanelEmitter.HIDE);
+}
+
+//获取模板列表
+export function getTplList(callback) {
+    if (!_.isFunction(callback)) return;
+
+    ajax.send({
+        url: TPL_URL
+    })
+        .done(result => {
+            callback(result);
+        })
+        .fail(err => {
+            message.error(err);
+            callback();
+        });
+}
+
+//保存模板
+export function saveTpl(callback) {
+    if (!_.isFunction(callback)) return;
+
+    ajax.send({
+        url: TPL_URL,
+        type: 'post'
+    })
+        .done(result => {
+            callback(result);
+        })
+        .fail(err => {
+            message.error(err);
+            callback();
+        });
 }
