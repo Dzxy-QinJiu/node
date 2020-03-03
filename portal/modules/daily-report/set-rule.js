571e7af506c59de09f11c45d37a41d26bab1fd3b
yubin <yubin@eefung.com>
2020-03-03 15:46:17 +0800

    显示团队下拉框

--- portal/modules/daily-report/set-rule.js
+++ portal/modules/daily-report/set-rule.js
@@ -6,11 +6,18 @@ import { Form, Button } from 'antd';
 import { VIEW_TYPE } from './consts';
 import { hideReportPanel } from './utils';
 import { renderFormItemFunc } from 'antc/lib/utils/form-utils';
+import { getMyTeamTreeAndFlattenList } from 'PUB_DIR/sources/utils/common-data-util';
 import addTplHoc from './add-tpl-hoc';
 
 class SetRule extends React.Component {
-    state = {
-        currentView: VIEW_TYPE.ADD_TPL,
+    componentDidMount() {
+        getMyTeamTreeAndFlattenList(data => {
+            if (!data.errorMsg) {
+                this.props.updateState({
+                    teamList: data.teamList
+                });
+            }
+        });
     }
 
     render() {
@@ -19,7 +26,17 @@ class SetRule extends React.Component {
         return (
             <div>
                 <Form onSubmit={this.handleSubmit}>
-                    {renderFormItem('谁可填写', 'name', {})}
+                    {renderFormItem('谁可填写', 'name', {
+                        type: 'select',
+                        options: _.map(this.props.teamList, item => ({name: item.group_name, value: item.group_id})),
+                        elementProps: {
+                            placeholder: '请选择团队'
+                        },
+                        fieldDecoratorOption: {
+                            rules: [{ required: true, message: '请选择团队' }]
+                        }
+                    })}
+
                 </Form>
             </div>
         );
