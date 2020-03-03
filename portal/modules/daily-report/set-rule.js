397dc21490a75bfba8fe1478d00dd3b336b1a2d3
yubin <yubin@eefung.com>
2020-03-03 09:22:21 +0800

    添加设置规则表单

--- portal/modules/daily-report/set-rule.js
+++ portal/modules/daily-report/set-rule.js
@@ -2,9 +2,10 @@
  * 设置规则
  */
 
-import { Button } from 'antd';
+import { Form, Button } from 'antd';
 import { VIEW_TYPE } from './consts';
 import { hideReportPanel } from './utils';
+import { renderFormItemFunc } from 'antc/lib/utils/form-utils';
 import addTplHoc from './add-tpl-hoc';
 
 class SetRule extends React.Component {
@@ -13,12 +14,17 @@ class SetRule extends React.Component {
     }
 
     render() {
+        const renderFormItem = renderFormItemFunc.bind(this, {});
+
         return (
             <div>
                 设置规则
+                <Form onSubmit={this.handleSubmit}>
+                    {renderFormItem('谁可填写', 'name', {})}
+                </Form>
             </div>
         );
     }
 }
 
-export default addTplHoc(SetRule);
+export default addTplHoc(Form.create()(SetRule));
