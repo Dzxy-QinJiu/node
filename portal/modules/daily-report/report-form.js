660bb21a350d83c0da7bc5ed35ddbb434b7d4b04
yubin <yubin@eefung.com>
2020-03-05 14:30:17 +0800

    填写报告页面显示保存等按钮

--- portal/modules/daily-report/report-form.js
+++ portal/modules/daily-report/report-form.js
@@ -5,7 +5,7 @@
 import { Form } from 'antd';
 import { renderFormItemFunc } from 'antc/lib/utils/form-utils';
 import { VIEW_TYPE } from './consts';
-import { renderButtonZoneFunc } from './utils';
+import { renderButtonZoneFunc, hideReportPanel } from './utils';
 
 class ReportForm extends React.Component {
     render() {
@@ -25,10 +25,76 @@ class ReportForm extends React.Component {
                         hide: !isPreview,
                         name: '返回',
                         func: () => { this.props.updateState({ currentView: VIEW_TYPE.ADD_TPL }); },
+                    }, {
+                        hide: isPreview,
+                        func: hideReportPanel,
+                        name: '取消',
+                    }, {
+                        hide: isPreview,
+                        func: this.save.bind(this),
+                        name: '保存',
+                    }, {
+                        hide: isPreview,
+                        func: this.submit.bind(this),
+                        name: '提交',
                     }])}
                 </Form>
             </div>
         );
+    }
+
+    save() {
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log(values);
+                _.each(values, (value, key) => {
+                    if (_.isUndefined(value)) delete values[key];
+                });
+
+                if (values.status === true) {
+                    values.status = 'on';
+                } else if (values.status === false) {
+                    values.status = 'off';
+                }
+
+                const { tplList, selectedTpl } = this.props;
+
+                const tplData = _.find(tplList, tpl => tpl.id === selectedTpl) || {};
+
+                const postData = _.extend({}, tplData, values);
+
+                console.log(postData);//return
+                saveTpl(postData, result => {});
+                //hideReportPanel()
+            }
+        });
+    }
+
+    submit() {
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log(values);
+                _.each(values, (value, key) => {
+                    if (_.isUndefined(value)) delete values[key];
+                });
+
+                if (values.status === true) {
+                    values.status = 'on';
+                } else if (values.status === false) {
+                    values.status = 'off';
+                }
+
+                const { tplList, selectedTpl } = this.props;
+
+                const tplData = _.find(tplList, tpl => tpl.id === selectedTpl) || {};
+
+                const postData = _.extend({}, tplData, values);
+
+                console.log(postData);//return
+                saveTpl(postData, result => {});
+                //hideReportPanel()
+            }
+        });
     }
 }
 
