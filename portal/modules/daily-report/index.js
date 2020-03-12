9c9a52a645b095af5085b7a433907f64e995298d
yubin <yubin@eefung.com>
2020-03-12 13:59:59 +0800

    使用分析模块里的顶部筛选

--- portal/modules/daily-report/index.js
+++ portal/modules/daily-report/index.js
@@ -1,6 +1,6 @@
 require('./style.less');
 import ReportList from './report-list';
-import ReportFilter from './report-filter';
+import ReportFilter from 'MOD_DIR/analysis/public/top-bar';
 import reportLayoutHoc from 'CMP_DIR/report-layout-hoc';
 
 module.exports = reportLayoutHoc(ReportList, ReportFilter);
