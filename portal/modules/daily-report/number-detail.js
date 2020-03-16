66d993da11562f02443f2d14eb9ffa87230509a5
yubin <yubin@eefung.com>
2020-03-16 17:33:09 +0800

    修改数字详情面板

--- portal/modules/daily-report/number-detail.js
+++ portal/modules/daily-report/number-detail.js
@@ -8,36 +8,14 @@ import { VIEW_TYPE } from './consts';
 import { renderButtonZoneFunc, hideReportPanel, getReportList, saveReport } from './utils';
 
 class NumberDetail extends React.Component {
-    componentDidMount() {
-        if (!this.props.isPreview) {
-            getReportList(list => {
-                const tplValues = _.first(list) || {};
-                this.props.updateState({ tplValues });
-            });
-        }
-    }
-
     render() {
-        const renderFormItem = renderFormItemFunc.bind(this, {});
-        const renderButtonZone = renderButtonZoneFunc.bind(this);
-
-        const { updateState, clickedTpl, isPreview, tplValues } = this.props;
-        const items = isPreview ? clickedTpl.items : tplValues.item_values;
-        const editableFields = ['其他'];
-        const editableItems = _.filter(items, item => _.includes(editableFields, item.name));
-        const unEditableItems = _.filter(items, item => !_.includes(editableFields, item.name));
+        const { currentReport } = this.props;
 
         return (
             <div>
-                <Form>
-                    {_.map(unEditableItems, item => {
-                        return renderFormItem(item.name, item.name, { type: 'text', initialValue: item.value });
-                    })}
-
-                    {_.map(editableItems, item => {
-                        return renderFormItem(item.name, item.name, { type: 'textarea', fieldDecoratorOption: { initialValue: item.value_str } });
-                    })}
-                </Form>
+                {_.map(currentReport.item_values, item => {
+                    return <div>{item.name}：{item.value}</div>;
+                })}
             </div>
         );
     }
