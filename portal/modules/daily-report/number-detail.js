c9da6e6d431797f5157ec78a3783eba66df3445e
yubin <yubin@eefung.com>
2020-03-25 15:35:01 +0800

    数字详情按设计图调整

--- portal/modules/daily-report/number-detail.js
+++ portal/modules/daily-report/number-detail.js
@@ -9,12 +9,37 @@ class NumberDetail extends React.Component {
         const { numberDetail } = this.props;
 
         return (
-            <div>
+            <div className="number-detail">
                 {_.map(numberDetail.detail, item => (
                     <DetailCard
                         content={(
                             <div>
-                                {item.customer_name}
+                                {/电话|通话/.test(numberDetail.name) ? (
+                                    <div className="call-detail">
+                                        <div className="call-time">
+                                            {moment(item.call_date).format(oplateConsts.HOUR_MUNITE_FORMAT)}
+                                        </div>
+                                        <div className="call-info">
+                                            <div className="contact-info">
+                                                <div className="contact-name">
+                                                    {item.contact_name}
+                                                </div>
+                                                {item.billsec ? (
+                                                    <div className="bill-sec">
+                                                        {item.billsec}秒
+                                                    </div>
+                                                ) : null}
+                                            </div>
+                                            <div className="customer-name">
+                                                {item.customer_name}
+                                            </div>
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <div className="customer-detail">
+                                        {item.customer_name}
+                                    </div>
+                                )}
                             </div>
                         )}
                     />
