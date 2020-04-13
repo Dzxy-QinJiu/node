59ada0f2c0edeb5aa4c2ec359570a04d75094208
yubin <yubin@eefung.com>
2020-04-13 11:04:10 +0800

    失败时关闭loading状态

--- portal/modules/app_user_manage/public/views/payment-records.js
+++ portal/modules/app_user_manage/public/views/payment-records.js
@@ -39,6 +39,8 @@ class PaymentRecords extends React.Component {
             })
             .fail(err => {
                 message.error(err);
+
+                this.setState({ loading: false });
             });
     };
 
