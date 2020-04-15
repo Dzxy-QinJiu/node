e9a72aba4ebfcf46ab3ca43f47f1f014b4a23b20
zhangshujuan <zhangshujuan@eefung.com>
2020-04-15 21:34:42 +0800

    误删除代码的恢复

--- portal/modules/apply_approve_list/public/all_application_type/user_apply/public/views/apply-view-detail.js
+++ portal/modules/apply_approve_list/public/all_application_type/user_apply/public/views/apply-view-detail.js
@@ -2585,13 +2585,12 @@ const COLUMN_WIDTH = {
                     obj.nick_name = this.state.formData.nick_name;
                 }
             }
-            console.log(obj);
-            // ApplyViewDetailActions.submitApply(obj, detailInfo.type, () => {
-            //     //调用父组件的方法进行审批完成后的其他处理
-            //     if (_.isFunction(this.props.afterApprovedFunc)) {
-            //         this.props.afterApprovedFunc();
-            //     }
-            // });
+            ApplyViewDetailActions.submitApply(obj, detailInfo.type, () => {
+                //调用父组件的方法进行审批完成后的其他处理
+                if (_.isFunction(this.props.afterApprovedFunc)) {
+                    this.props.afterApprovedFunc();
+                }
+            });
         };
         var validation = this.refs.validation;
         if (!validation) {
