9a72fe8687231134ccff7be69e65101e067997ef
wangzhentao <wangzhentao@curtao.com>
2020-04-16 16:54:00 +0800

    添加口罩厂家

--- i18n/en_US.js
+++ i18n/en_US.js
@@ -3954,6 +3954,7 @@ var en_US = {
     'clue.recommend.listed': 'Listed companies',//上市企业
     'clue.recommend.register.half.year': 'Last half year registration',//最近半年注册
     'clue.recommend.extract.count.more': '100 million + clues for you to extract',//1亿+条线索待您提取
+    'clue.recommend.mask.Manufactor': 'Mask manufacturers',// 口罩厂家
     // 线索分配策略
     'clue.assignment.strategy.add': 'Add assignment strategy',//添加分配策略
     'clue.assignment.strategy.fail.to.get': 'Fail to fetch lead assignment strategy list',//获取线索分配策略列表失败
