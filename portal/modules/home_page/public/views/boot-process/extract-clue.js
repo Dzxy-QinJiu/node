2812e13328be3b42da230df24471621cf3451124
wangzhentao <wangzhentao@curtao.com>
2020-04-17 14:34:16 +0800

    node端添加字段

--- portal/modules/home_page/public/views/boot-process/extract-clue.js
+++ portal/modules/home_page/public/views/boot-process/extract-clue.js
@@ -932,6 +932,7 @@ class ExtractClues extends React.Component {
                             const otherCls = classNames('extract-clue-text__info extract-clue-text__filters', {
                                 'extract-clue-text__null': !otherProps.products.hasHighLight && !otherProps.scope.hasHighLight && !otherProps.industry.hasHighLight && !otherProps.companyProfile.hasHighLight
                             });
+                            item.labels = item.labels.concat(item.features);
                             return (
                                 <div className={cls} key={item.id} onClick={this.handleClickClueName.bind(this, item)}>
                                     <Checkbox checked={this.hasChecked(item)} disabled={this.getDisabledClue(item)} onChange={this.handleCheckChange.bind(this, item)}/>
