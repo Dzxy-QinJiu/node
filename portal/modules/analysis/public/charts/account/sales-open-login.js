6607ed53592147a9f84d578c4f9578a77d6b7187
yubin <yubin@eefung.com>
2020-04-17 17:21:01 +0800

    修复未选择过应用时查看开通用户登陆统计不显示应用列的问题

--- portal/modules/analysis/public/charts/account/sales-open-login.js
+++ portal/modules/analysis/public/charts/account/sales-open-login.js
@@ -55,7 +55,7 @@ export function getSalesOpenAccountLoginChart() {
             const storedAppId = storageUtil.local.get(STORED_APP_ID_KEY);
 
             //选择全部或多个应用时显示应用名
-            if (storedAppId === 'all' || _.includes(storedAppId, ',')) {
+            if (!storedAppId || storedAppId === 'all' || _.includes(storedAppId, ',')) {
                 const appColumn = {
                     title: Intl.get('common.product','产品'),
                     dataIndex: 'app_name',
