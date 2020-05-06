0a3db1cb65ada8226b20e99652d4b4525177612a
yubin <yubin@eefung.com>
2020-05-06 17:16:54 +0800

    本月试用合格组成统计去掉本月净增值

--- portal/modules/analysis/public/charts/customer/trial-qualified-compose.js
+++ portal/modules/analysis/public/charts/customer/trial-qualified-compose.js
@@ -22,7 +22,6 @@ export function getCustomerTrialQualifiedComposeChart() {
                 Intl.get('common.this.month.new', '本月新增'),
                 Intl.get('common.this.month.back', '本月回流'),
                 Intl.get('common.this.month.lose', '本月流失'),
-                Intl.get('common.this.month.add', '本月比上月净增'),
                 Intl.get('common.this.month', '本月'),
             ]);
 
@@ -47,12 +46,6 @@ export function getCustomerTrialQualifiedComposeChart() {
             let thisMonthLoseNum = _.sumBy(data, 'this_month_lose.total');
             //本月回流
             let thisMonthBackNum = _.sumBy(data, 'this_month_back.total');
-            //本月净增
-            let thisMonthAddNum = _.sumBy(data, 'this_month_add.total');
-            
-            if (thisMonthAddNum < 0) {
-                _.set(option, 'xAxis[0].data[4]', Intl.get('common.this.month.reduce', '本月比上月减少'));
-            }
 
             //本月新增数据辅助，用于实现阶梯瀑布效果，默认以上月数据为基准
             let thisMonthNewNumAssist = lastMonthNum;
@@ -71,17 +64,6 @@ export function getCustomerTrialQualifiedComposeChart() {
             //本月流失数辅助值为本月回流数辅助值与本月回流数之和再减去本月流失数
             let thisMonthLoseNumAssist = thisMonthBackNumAssist + thisMonthBackNum - thisMonthLoseNum;
 
-            //本月净增数辅助值默认为上月个数
-            let thisMonthAddNumAssist = lastMonthNum;
-
-            //如果本月净增数为负值
-            if (thisMonthAddNum < 0) {
-                //则本月净增数辅助值为上月个数与本月净增之和，也即上月个数减去本月净增的绝对值
-                thisMonthAddNumAssist = lastMonthNum + thisMonthAddNum;
-                //将本月净增数设为其绝对值，以避免柱子显示在横轴下方
-                thisMonthAddNum = Math.abs(thisMonthAddNum);
-            }
-
             //辅助系列，会在堆积的柱子中占空间，但不会显示出来，这样就能呈现出阶梯瀑布效果了
             let serieAssist = _.extend({}, serie, {
 
@@ -96,7 +78,7 @@ export function getCustomerTrialQualifiedComposeChart() {
                         color: 'rgba(0,0,0,0)'
                     }
                 },
-                data: ['-', thisMonthNewNumAssist, thisMonthBackNumAssist, thisMonthLoseNumAssist, thisMonthAddNumAssist, '-'],
+                data: ['-', thisMonthNewNumAssist, thisMonthBackNumAssist, thisMonthLoseNumAssist, '-'],
             });
 
             //上月系列
@@ -110,7 +92,7 @@ export function getCustomerTrialQualifiedComposeChart() {
             let serieThisMonth = _.extend({}, serie, {
                 name: Intl.get('common.this.month', '本月'),
                 //数据中只有本月相关数据为实际值，其他的均为空值，在堆积时会用到
-                data: ['-', thisMonthNewNum, thisMonthBackNum, thisMonthLoseNum, thisMonthAddNum, thisMonthNum],
+                data: ['-', thisMonthNewNum, thisMonthBackNum, thisMonthLoseNum, thisMonthNum],
             });
 
             option.series = [
