41b01bbab5b454b56137439bab755e8c204fd5ce
zhangshujuan <zhangshujuan@eefung.com>
2020-05-20 11:28:09 +0800

    删除打印日志

--- portal/public/sources/push/index.js
+++ portal/public/sources/push/index.js
@@ -1120,7 +1120,6 @@ function applyUnreadReplyListener(unreadReply) {
     //将未读回复列表分用户存入sessionStorage（session失效时会自动清空数据）
     let unreadMyReplyList = session.get(MY_UNREAD_REPLY);
     //member_id 推送给的人的id，需要校验的是这条审批的申请人的id是不是当前登录人，如果是，推送到我的申请这个tab
-    console.log(userData.getUserData().user_id);
     if (unreadMyReplyList && userData.getUserData().user_id === _.get(unreadReply,'applicateId')) {
         unreadMyReplyList = JSON.parse(unreadMyReplyList);
         //已有回复列表，将新得回复加入回复列表中
