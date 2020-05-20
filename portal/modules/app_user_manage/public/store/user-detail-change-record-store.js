4de659432c036040b1b2662afd53204d17da4900
huzhenli <huzhenli@eefung.com>
2020-05-20 16:59:39 +0800

    用户详情中，变更记录，应用选择用appId以及选择框提到滚动区域外的修改

--- portal/modules/app_user_manage/public/store/user-detail-change-record-store.js
+++ portal/modules/app_user_manage/public/store/user-detail-change-record-store.js
@@ -15,7 +15,7 @@ UserDetailChangeRecordStore.prototype.resetState = function() {
     this.changeRecordLoading = true;//是否正在加载变更记录
     this.changeRecordErrMsg = '';//加载错误后的提示信息
     this.page_size = 200;//加载记录每页的条数
-    this.app = '';//默认显示的app名称
+    this.selectedAppId = '';// 选择应用的id，默认空
 };
 UserDetailChangeRecordStore.prototype.getUserDetailChangeRecord = function(result) {
     if (result.loading){
@@ -31,8 +31,8 @@ UserDetailChangeRecordStore.prototype.getUserDetailChangeRecord = function(resul
     }
 };
 
-UserDetailChangeRecordStore.prototype.setApp = function(app) {
-    this.app = app;
+UserDetailChangeRecordStore.prototype.setApp = function(selectedAppId) {
+    this.selectedAppId = selectedAppId;
 };
 
 //使用alt导出store
