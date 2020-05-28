c16ff9362fb1bba36a7de0bd77e78d69ed67c212
zhangshujuan <zhangshujuan@eefung.com>
2020-05-28 11:17:43 +0800

    申请审批列表中的样式调整

--- portal/modules/apply_approve_list/public/ajax/apply_approve_list_ajax.js
+++ portal/modules/apply_approve_list/public/ajax/apply_approve_list_ajax.js
@@ -14,7 +14,7 @@ exports.getApplyListStartSelf = function(obj) {
         type: 'get',
         data: obj,
         success: function(data) {
-            Deferred.resolve(data);
+            Deferred.resolve([]);
         },
         error: function(data,textStatus) {
             if(textStatus !== 'abort') {
