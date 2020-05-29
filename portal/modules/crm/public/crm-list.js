3238c22fe97d72498a495ce049f9b652665eeba9
huzhenli <huzhenli@eefung.com>
2020-05-29 10:49:12 +0800

    自定义配置，客户表格处理以及详情传参修改

--- portal/modules/crm/public/crm-list.js
+++ portal/modules/crm/public/crm-list.js
@@ -1,4 +1,3 @@
-var React = require('react');
 require('./css/index.less');
 import { Tag, Modal, message, Button, Icon, Dropdown, Menu, Popconfirm, Popover, Input, Radio } from 'antd';
 import { AntcTable } from 'antc';
@@ -2356,6 +2355,36 @@ class Crm extends React.Component {
                 }
             }
         ];
+        let customerCustomFieldData = this.props.customerCustomFieldData;
+        if (!_.isEmpty(customerCustomFieldData)) {
+            // 客户自定义字段
+            const customizedVariables = _.get(customerCustomFieldData, '[0].customized_variables');
+            _.map(customizedVariables, item => {
+                const name = _.get(item, 'name');
+                const dataIndex = _.get(item, 'key');
+                if (_.get(item, 'need_show')) {
+                    let customColumn = {
+                        title: name,
+                        width: '150px',
+                        dataIndex: dataIndex,
+                        render: (text, record, index) => {
+                            let customVariables = _.get(record, 'custom_variables');
+                            return (
+                                <div>
+                                    {
+                                        customVariables ? customVariables[dataIndex] : ''
+                                    }
+                                </div>
+                            );
+                        }
+                    };
+                    if (_.get(item, 'need_sort')) {
+                        customColumn.sorter = true;
+                    }
+                    columns.push(customColumn);
+                }
+            });
+        }
         //csm.curtao.com域名下不展示订单
         if (isCurtao()) {
             columns = _.filter(columns, column => column.title !== Intl.get('user.apply.detail.order', '订单'));
@@ -2566,6 +2595,7 @@ Crm.propTypes = {
     showCustomerRecycleBin: PropTypes.func,
     showCustomerPool: PropTypes.func,
     isExtractSuccess: PropTypes.bool,
+    customerCustomFieldData: PropTypes.object,
 };
 
 module.exports = Crm;
--- portal/modules/crm/public/index.js
+++ portal/modules/crm/public/index.js
@@ -6,6 +6,7 @@ import Trace from 'LIB_DIR/trace';
 import CustomerRecycleBin from './views/customer-recycle-bin';
 import CustomerPool from './views/customer-pool';
 import {crmEmitter} from 'OPLATE_EMITTER';
+import customFieldAjax from '../../custom_field_manage/public/ajax';
 //各视图类型常量
 const VIEW_TYPE = {
     CUSTOMER: 'customer',//客户列表视图
@@ -17,9 +18,24 @@ class CrmIndex extends React.Component {
     state = {
         customerViewType: VIEW_TYPE.CUSTOMER,//客户展示的视图
         crmSearchCondition: {},//客户列表中搜索的筛选条件
+        customerCustomFieldData: {}, // 客户自定义字段的值，默认空
     };
 
+    // 获取客户自定义字段信息
+    getCustomFieldConfig() {
+        const queryObj = {
+            customized_type: 'customer'
+        };
+        customFieldAjax.getCustomFieldConfig(queryObj).then( (result) => {
+            this.setState({
+                customerCustomFieldData: result
+            });
+        } );
+    }
+
     componentDidMount() {
+        // 获取客户自定义字段信息
+        this.getCustomFieldConfig();
         crmEmitter.on(crmEmitter.OPEN_VIEW_PANEL, this.handelCrmViewChange);
         if(_.get(this.props, 'history.action') === 'PUSH') {
             let paramsObj = _.get(this.props, 'location.state.paramsObj');
@@ -84,10 +100,14 @@ class CrmIndex extends React.Component {
             case VIEW_TYPE.CUSTOMER:
                 currView = (
                     <div data-tracename="客户管理">
-                        <CrmList {...this.props} showRepeatCustomer={this.showRepeatCustomer}
+                        <CrmList
+                            {...this.props}
+                            showRepeatCustomer={this.showRepeatCustomer}
                             isExtractSuccess={this.state.isExtractSuccess}
                             showCustomerPool={this.showCustomerPool}
-                            showCustomerRecycleBin={this.showCustomerRecycleBin}/>
+                            showCustomerRecycleBin={this.showCustomerRecycleBin}
+                            customerCustomFieldData={this.state.customerCustomFieldData}
+                        />
                     </div>);
                 break;
             case VIEW_TYPE.REPEAT_CUSTOMER:
