4bb5618fe7a8945bb32847af1956c5fd57a5547c
tangmaoqin <tangmaoqin@eefung.com>
2020-06-05 10:49:37 +0800

    法人，联系人展示样式调整

--- portal/modules/clue_customer/public/views/recomment_clues/index.js
+++ portal/modules/clue_customer/public/views/recomment_clues/index.js
@@ -96,9 +96,6 @@ const CHECK_PHONE_CONFIG = {
     SHOW_CHECK_PHONE_FEATURE_POP_TIP: 'first_show_check_phone_feature_pop_tip',
 };
 
-//最大联系人高度
-const MAX_PHONE_HEIGHT = 202;
-
 class RecommendCluesList extends React.Component {
     constructor(props) {
         super(props);
@@ -1906,66 +1903,60 @@ class RecommendCluesList extends React.Component {
     };
 
     renderContactBlock(item) {
-        let legalPerson = item.legalPerson;
         let contacts = item.contacts;
-        if(!legalPerson && !contacts.length) {
+        if(!contacts.length) {
             return null;
         }else {
-            let contact = {name: legalPerson, position: Intl.get('clue.recommend.legalperson', '法人')};
-            if(!legalPerson) {
-                contact = _.find(contacts, contactItem => contactItem.position === '法人') || _.get(contacts, '[0]');
-            }
-            let hasMoreContacts = contacts.length > 1;
-            let contentCls = classNames('more-contacts-wrapper',{
-                'has-scroll': hasMoreContacts > 2
+            let title = (
+                <div className="check-phone-title" data-tracename="更多联系人title内容">
+                    <div className="check-phone-title-content">
+                        <i className="iconfont icon-app-view"/>
+                        <span className="more-contacts-title">{Intl.get('clue.more.contacts.tip', '提取后可查看完整信息')}</span>
+                    </div>
+                    <i className="iconfont icon-close" data-tracename="点击关闭" title={Intl.get('common.app.status.close', '关闭')} onClick={this.handleContactPopoverChange.bind(this, false)}/>
+                </div>
+            );
+            let contentCls = classNames('more-contacts-wrapper', {
+                'has-scroll': contacts.length > 2
             });
+            let content = (
+                <div className={contentCls}>
+                    <div className="more-contacts-content">
+                        {_.map(contacts, contactItem => {
+                            return (
+                                <div key={contactItem.id} className="more-contacts-item">
+                                    <div className="more-contacts-name-content">
+                                        <span className="more-contacts-name">{contactItem.name}</span>
+                                        {contactItem.position ? <span className="more-contacts-position">({contactItem.position})</span> : null}
+                                    </div>
+                                    <div className="more-contacts-phones">
+                                        {_.map(contactItem.phone, phone => (
+                                            <div key={phone} className="more-contacts-phone-item">{phone}</div>
+                                        ))}
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            );
             return (
                 <span className='extract-clue-info-item'>
                     <span className="extract-clue-text-label">{Intl.get('call.record.contacts', '联系人')}：</span>
-                    <span>{contact.name}{contact.position ? `(${contact.position})` : null}</span>
-                    {hasMoreContacts ? (
-                        <Popover
-                            trigger="click"
-                            placement="bottomLeft"
-                            title={(
-                                <div className="check-phone-title" data-tracename="更多联系人title内容">
-                                    <div className="check-phone-title-content">
-                                        <i className="iconfont icon-app-view"/>
-                                        <span className="more-contacts-title">{Intl.get('clue.more.contacts.tip', '提取后可查看完整信息')}</span>
-                                    </div>
-                                    <i className="iconfont icon-close" data-tracename="点击关闭" title={Intl.get('common.app.status.close', '关闭')} onClick={this.handleContactPopoverChange.bind(this, false)}/>
-                                </div>
-                            )}
-                            content={(
-                                <div className={contentCls} style={{height: hasMoreContacts > 2 ? MAX_PHONE_HEIGHT : 'auto'}}>
-                                    <GeminiScrollbar>
-                                        <div className="more-contacts-content">
-                                            {_.map(contacts, contactItem => {
-                                                return (
-                                                    <div key={contactItem.id} className="more-contacts-item">
-                                                        <div className="more-contacts-name-content">
-                                                            <span className="more-contacts-name">{contactItem.name}</span>
-                                                            {contactItem.position ? <span className="more-contacts-position">({contactItem.position})</span> : null}
-                                                        </div>
-                                                        <div className="more-contacts-phones">
-                                                            {_.map(contactItem.phone, phone => (
-                                                                <div key={phone} className="more-contacts-phone-item">{phone}</div>
-                                                            ))}
-                                                        </div>
-                                                    </div>
-                                                );
-                                            })}
-                                        </div>
-                                    </GeminiScrollbar>
-                                </div>
-                            )}
-                            visible={this.state.showContactPopvisible === item.id}
-                            onVisibleChange={this.handleContactPopoverChange}
-                            overlayClassName="extract-limit-content check-phone-result-container more-contacts-container"
-                        >
-                            <span data-tracename="点击更多联系人" onClick={this.handleClickContactPopover.bind(this, item)} className="more-contacts">{Intl.get('clue.more.contacts', '更多联系人')}</span>
-                        </Popover>
-                    ) : null}
+                    <Popover
+                        trigger="click"
+                        placement="bottomLeft"
+                        title={title}
+                        content={content}
+                        visible={this.state.showContactPopvisible === item.id}
+                        onVisibleChange={this.handleContactPopoverChange}
+                        overlayClassName="extract-limit-content check-phone-result-container more-contacts-container"
+                    >
+                        <span className="clickable" data-tracename="点击联系人个数" onClick={this.handleClickContactPopover.bind(this, item)}>
+                            <span className="more-contacts">{_.get(contacts, 'length')}</span>
+                            {Intl.get('contract.22', '个')}
+                        </span>
+                    </Popover>
                 </span>
             );
         }
@@ -2040,7 +2031,12 @@ class RecommendCluesList extends React.Component {
                                         </div>
                                         <div className="extract-clue-text__filters">
                                             <div className="extract-clue-text-item">
-                                                {this.renderContactBlock(item)}
+                                                {item.legalPerson ? (
+                                                    <span className='extract-clue-info-item'>
+                                                        <span className="extract-clue-text-label">{Intl.get('clue.recommend.legalperson', '法人')}：</span>
+                                                        <span>{item.legalPerson}</span>
+                                                    </span>
+                                                ) : null}
                                                 {item.startTime ? (
                                                     <span className='extract-clue-info-item'>
                                                         <span className="extract-clue-text-label">{Intl.get('clue.customer.register.time', '注册时间')}：</span>
@@ -2051,12 +2047,13 @@ class RecommendCluesList extends React.Component {
                                                     item.capital ? (
                                                         <span className='extract-clue-info-item'>
                                                             <span className="extract-clue-text-label">{Intl.get('clue.recommend.registered.capital', '注册资本')}：</span>
-                                                            <span>{Intl.get('crm.149', '{num}万', {num: item.capital / 10000})}</span>
+                                                            <span>{Intl.get('crm.149', '{num}万', {num: (item.capital / 10000).toFixed(2)})}</span>
                                                         </span>
                                                     ) : null
                                                 }
                                             </div>
                                             <div className="extract-clue-text-item">
+                                                {this.renderContactBlock(item)}
                                                 {_.get(item.contact, 'phones') ? (
                                                     <span className="extract-clue-contacts-item">
                                                         <span className="extract-clue-text-label">{Intl.get('common.phone', '电话')}：</span>
