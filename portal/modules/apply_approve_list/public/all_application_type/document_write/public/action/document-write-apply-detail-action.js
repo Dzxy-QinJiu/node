260ffd06068b8f1c1a3aa11a82e7938594c90528
zhangshujuan <zhangshujuan@eefung.com>
2020-06-13 20:17:58 +0800

    错误信息处理

--- portal/modules/apply_approve_list/public/all_application_type/document_write/public/action/document-write-apply-detail-action.js
+++ portal/modules/apply_approve_list/public/all_application_type/document_write/public/action/document-write-apply-detail-action.js
@@ -79,7 +79,7 @@ function ApplyViewDetailActions() {
                 _.isFunction(callback) && callback();
                 changeApplyStatusPassOrReject(obj,data);
             }else{
-                this.dispatch({loading: false, error: true, errorMsg: Intl.get('errorcode.19', '审批申请失败')});
+                this.dispatch({loading: false, error: true, errorMsg: Intl.get('fail.apply.approve.result', '审批失败')});
             }
 
         }, (errorMsg) => {
