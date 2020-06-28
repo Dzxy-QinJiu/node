1b41eba8c2dff166d6da94f5f7965efbb09f9495
zhangshujuan <zhangshujuan@eefung.com>
2020-06-28 13:33:03 +0800

    小程序注册页点击登录的时候返回登录页面而非首页

--- portal/public/sources/utils/register_util.js
+++ portal/public/sources/utils/register_util.js
@@ -1,6 +1,6 @@
 //不要抽到别的文件中去，因为服务器端的注册页面会用，抽到别的文件中如果引用其他的文件，可能会报错
 var wx = require('weixin-js-sdk');
-exports.pcAndWechatMiniProgram = function(hrefUrl) {
+exports.pcAndWechatMiniProgram = function(hrefUrl,isRegistry) {
     if (window.__wxjs_environment === 'miniprogram') {//是在小程序包裹的web-view中的退出处理
         $.ajax({
             url: '/logout',
@@ -8,7 +8,11 @@ exports.pcAndWechatMiniProgram = function(hrefUrl) {
             type: 'get',
             data: {isWechatLogout: true},
         });
-        wx.miniProgram.navigateBack();
+        if(_.isBoolean(isRegistry) && isRegistry){
+            wx.miniProgram.redirectTo({url: '/pages/login/index'});
+        }else{
+            wx.miniProgram.navigateBack();
+        }
     } else {
         window.location.href = hrefUrl;
     }
