5609c5ff04051c24c318420bdab25a203fe189c4
yubin <yubin@eefung.com>
2020-07-01 09:35:54 +0800

    通话时长分布统计接口调通

--- portal/modules/analysis/public/charts/call/call-duration-distribution.js
+++ portal/modules/analysis/public/charts/call/call-duration-distribution.js
@@ -2,8 +2,17 @@
  * 通话时长分布统计
  */
 
+//时间区间
+const TIME_INTERVAL = {
+    '0.0': '通话时长小于30秒的通话个数',
+    '30.0': '通话时长30-60秒的通话个数',
+    '60.0': '通话时长60-90秒的通话个数',
+    '90.0': '通话时长90-120秒的通话个数',
+    '120.0': '通话时长大于120秒的通话个数',
+};
+
 export function getCallDurationDistributionChart(paramObj = {}) {
-    let title = Intl.get('analysis.call.record.statistics', '通话时长分布统计');
+    let title = '通话时长分布统计';
 
     return {
         title,
@@ -11,19 +20,44 @@ export function getCallDurationDistributionChart(paramObj = {}) {
         height: 'auto',
         chartType: 'table',
         url: '/rest/analysis/callrecord/v1/callrecord/billsec/histogram',
-        option: {
-            columns: [
-                {
+        dataField: 'list',
+        processData: data => {
+            _.each(data, item => {
+                _.each(item.billsec_list, billsecItem => {
+                    item[billsecItem.name] = billsecItem.count;
+                });
+            });
+
+            return data;
+        },
+        processOption: (option, chart) => {
+            let columns = [];
+            option.columns = columns;
+
+            const hasNickName = _.chain(chart.data).first().has('nick_name').value();
+
+            if (hasNickName) {
+                columns.push({
                     title: '成员',
-                    dataIndex: 'dst',
+                    dataIndex: 'nick_name',
                     width: 120,
-                }, {
-                    title: Intl.get('sales.home.call.top.ten', '单次通话时长分布'),
-                    dataIndex: 'billsec',
-                    width: 100,
+                });
+            }
+
+            columns.push({
+                title: '团队',
+                dataIndex: 'sales_team',
+                width: 120,
+            });
+
+            _.each(TIME_INTERVAL, (value, key) => {
+                columns.push({
+                    title: value,
+                    dataIndex: key,
+                    width: 120,
                     align: 'right',
-                }
-            ]
+                });
+            });
         }
     };
 }
