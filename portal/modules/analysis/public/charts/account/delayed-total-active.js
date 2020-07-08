1cb0f9e8d23ae1e7ba01a61a5a08b4033e801523
yubin <yubin@eefung.com>
2020-07-08 17:51:09 +0800

    国际化

--- portal/modules/analysis/public/charts/account/delayed-total-active.js
+++ portal/modules/analysis/public/charts/account/delayed-total-active.js
@@ -6,7 +6,7 @@ import { MERIDIEM } from 'PUB_DIR/sources/utils/consts';
 
 export function getDelayedTotalActiveChart() {
     return {
-        title: '登录用户统计',
+        title: Intl.get('analysis.login.user.statistics', '登录用户统计'),
         chartType: 'table',
         url: '/rest/analysis/user/v3/:data_type/delay/user/team',
         option: {
@@ -19,11 +19,11 @@ export function getDelayedTotalActiveChart() {
                 dataIndex: 'member_name',
                 width: 100,
             }, {
-                title: '延期总数',
+                title: Intl.get('analysis.the.total.number.of.delay', '延期总数'),
                 dataIndex: 'total',
                 width: 100,
             }, {
-                title: '活跃数',
+                title: Intl.get('operation.report.active.num', '活跃数'),
                 dataIndex: 'active',
                 width: 100,
             }],
