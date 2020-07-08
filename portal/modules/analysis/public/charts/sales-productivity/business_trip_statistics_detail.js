e41f108a8335de83611109517b4caa5a07e667fc
yubin <yubin@eefung.com>
2020-07-08 15:17:05 +0800

    销售生产力分析中，增加出差统计详情表

---
+++ portal/modules/analysis/public/charts/sales-productivity/business_trip_statistics_detail.js
@@ -0,0 +1,62 @@
+/**
+ * 出差统计详情
+ */
+
+import { MERIDIEM } from 'PUB_DIR/sources/utils/consts';
+
+export function getBusinessTripStatisticsDetailChart() {
+    return {
+        title: '出差统计详情',
+        chartType: 'table',
+        layout: {sm: 24},
+        height: 'auto',
+        url: '/rest/base/v1/workflow/businesstrip/customervisit/list',
+        processData: data => {
+            _.each(data, item => {
+                item.visit_record_time = moment(item.visit_record_time).format(oplateConsts.DATE_FORMAT);
+                if (item.visit_start === item.visit_end) {
+                    item.visit_time = transAmPm(item.visit_start);
+                } else {
+                    item.visit_time = transAmPm(item.visit_start) + ' - ' + transAmPm(item.visit_end);
+                }
+            });
+
+            return data;
+        },
+        option: {
+            columns: [{
+                title: '销售',
+                dataIndex: 'nickname',
+                width: 100,
+            }, {
+                title: '团队',
+                dataIndex: 'team_name',
+                width: 100,
+            }, {
+                title: '客户名',
+                dataIndex: 'customer_name',
+                width: 100,
+            }, {
+                title: '拜访时间',
+                dataIndex: 'visit_time',
+                width: 100,
+            }, {
+                title: '备注',
+                dataIndex: 'remarks',
+                width: 100,
+            }, {
+                title: '跟进记录内容',
+                dataIndex: 'visit_record',
+                width: 100,
+            }, {
+                title: '跟进记录时间',
+                dataIndex: 'visit_record_time',
+                width: 100,
+            }],
+        }
+    };
+
+    function transAmPm(dateStr = '') {
+        return dateStr.replace('_', '').replace('AM', MERIDIEM.AM).replace('PM', MERIDIEM.PM);
+    }
+}
